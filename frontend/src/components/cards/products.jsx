dcff579917f9fb8c8475ca90eee2b8af7415cfbc
Taiwo Olapade <semiteprofessor@gmail.com>
2025-03-09 12:30:28 +0100

    Commit

--- frontend/src/components/cards/products.jsx
+++ frontend/src/components/cards/products.jsx
@@ -24,47 +24,52 @@ import {
 } from "@mui/material";
 
 // components
-import { useDispatch } from "src/redux/store";
-import { setWishlist } from "src/redux/slices/wishlist";
+import { useDispatch } from "@/redux/store";
+import { setWishlist } from "@/redux/slices/wishlist";
 import {
   addCompareProduct,
   removeCompareProduct,
 } from "../../redux/slices/compare";
-import ColorPreviewGroup from "src/components/colorPreviewGroup";
+import ColorPreviewGroup from "@/components/colorPreviewGroup";
 
-import Label from "src/components/label";
-import BlurImage from "src/components/blurImage";
+import Label from "@/components/label";
+import BlurImage from "@/components/blurImage";
+
 // hooks
-import { useCurrencyConvert } from "src/hooks/convertCurrency";
-import { useCurrencyFormatter } from "src/hooks/formatCurrency";
+import { useCurrencyConvert } from "@/hooks/convertCurrency";
+import { useCurrencyFormatter } from "@/hooks/formatCurrency";
+
 // api
-import * as api from "src/services";
+import * as api from "@/services";
+
 // icons
 import { IoMdHeartEmpty } from "react-icons/io";
 import { GoEye } from "react-icons/go";
 import { GoGitCompare } from "react-icons/go";
 import { IoIosHeart } from "react-icons/io";
 import { FaRegStar } from "react-icons/fa";
+
 // dynamic
-const ProductDetailsDialog = dynamic(() => import("../dialog/productDetails"));
-const ShopProductCard = ({...props}) => {
-    const { product, loading } = props;
-    const cCurrency = useCurrencyConvert();
-    const fCurrency = useCurrencyFormatter();
+const ProductDetailsDialog = dynamic(() => import("../dialog/"));
+const ShopProductCard = ({ ...props }) => {
+  const { product, loading } = props;
+  const cCurrency = useCurrencyConvert();
+  const fCurrency = useCurrencyFormatter();
 
-    const [open, setOpen] = useState(false);
-    const [openActions, setOpenActions] = useState(false);
-    const theme = useTheme();
-    const router = useRouter();
-    const dispatch = useDispatch();
-    // type error
-    const { wishlist } = useSelector(({ wishlist }) => wishlist);
-    const { products: compareProducts } = useSelector(({ compare }) => compare);
+  const [open, setOpen] = useState(false);
+  const [openActions, setOpenActions] = useState(false);
+  const theme = useTheme();
+  const router = useRouter();
+  const dispatch = useDispatch();
 
-    const { isAuthenticated } = useSelector(({ user }) => user);
-    const isTablet = useMediaQuery("(max-width:900px)");
-    const [isLoading, setLoading] = useState(false);
-  
+  // type error
+  const { wishlist } = useSelector(({ wishlist }) => wishlist);
+  const { products: compareProducts } = useSelector(({ compare }) => compare);
+
+  const { isAuthenticated } = useSelector(({ user }) => user);
+  const isTablet = useMediaQuery("(max-width:900px)");
+  const [isLoading, setLoading] = useState(false);
+
   return <div>ShopProductCard</div>;
 };
 
