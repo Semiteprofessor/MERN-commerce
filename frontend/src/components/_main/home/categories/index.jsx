8236257a0cad40e4e71ef49504a8c319b14a43f3
Taiwo Olapade <semiteprofessor@gmail.com>
2025-03-10 11:46:33 +0100

    Commit

--- frontend/src/components/_main/home/categories/index.jsx
+++ frontend/src/components/_main/home/categories/index.jsx
@@ -12,6 +12,9 @@ import * as api from "@/services";
 import { useQuery } from "react-query";
 
 const Categories = () => {
+    const { data, isLoading } = useQuery(["get-home-categories"], () =>
+      api.getHomeCategories()
+    );
   return (
     <Paper elevation={0}>
       <Stack
@@ -20,7 +23,88 @@ const Categories = () => {
           gap: 3,
           mt: 5,
         }}
-      ></Stack>
+      >
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          textAlign={{ xs: "center", md: "left" }}
+          alignItems="center"
+        >
+          <Box>
+            <Typography variant="h2" color="text.primary">
+              Top Categories
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting
+              Industry.
+            </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            sx={{
+              borderRadius: 6,
+              display: { xs: "none", md: "flex" },
+              minWidth: 130,
+              px: 1,
+            }}
+            endIcon={<IoIosArrowForward />}
+            component={NextLink}
+            href={`/categories`}
+          >
+            View More
+          </Button>
+        </Stack>
+        <Box>
+          <Grid
+            container
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            {(isLoading ? Array.from(new Array(6)) : (data?.data ?? [])).map(
+              (inner, idx) => (
+                <React.Fragment key={inner?._id || idx}>
+                  <Grid item lg={2} md={3} sm={4} xs={4}>
+                    <CategoryCard category={inner} isLoading={isLoading} />
+                  </Grid>
+                </React.Fragment>
+              )
+            )}
+
+            {!isLoading && (!data?.data || data.data.length === 0) && (
+              <Typography
+                variant="h3"
+                color="error.main"
+                textAlign="center"
+                width="100%"
+                mt={2}
+              >
+                Categories not found
+              </Typography>
+            )}
+          </Grid>
+
+          <Button
+            variant="text"
+            color="primary"
+            size="small"
+            sx={{
+              borderRadius: 6,
+              mx: "auto",
+              mt: 3,
+              display: { md: "none", xs: "flex" },
+              maxWidth: "120px",
+            }}
+            endIcon={<IoIosArrowForward />}
+            component={NextLink}
+            href={`/categories`}
+          >
+            View More
+          </Button>
+        </Box>
+      </Stack>
     </Paper>
   );
 };
