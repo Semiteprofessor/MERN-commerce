51ebc0fbd5e1079303c34909afe27b60bccc5be1
Taiwo Olapade <semiteprofessor@gmail.com>
2025-03-10 11:17:11 +0100

    Commit

--- frontend/src/app/(user)/page.jsx
+++ frontend/src/app/(user)/page.jsx
@@ -1,10 +1,33 @@
 import { Container } from "@mui/material";
 import React from "react";
 
+// Dynamic imports
+const Categories = dynamic(() => import("@/components/_main/home/categories"));
+const BestSellingProducs = dynamic(
+  () => import("@/components/_main/home/bestSelling")
+);
+const Banner = dynamic(() => import("@/components/_main/home/banner"));
+const Brands = dynamic(() => import("@/components/_main/home/brands"));
+const TopCollection = dynamic(() => import("@/components/_main/home/top"));
+const Shops = dynamic(() => import("@/components/_main/home/shop"));
+const Compaigns = dynamic(() => import("@/components/_main/home/compaign"));
+const Testimonials = dynamic(
+  () => import("@/components/_main/home/testimonials")
+);
+const FeaturedProducts = dynamic(
+  () => import("@/components/_main/home/featured")
+);
+const SubscriptionModal = dynamic(
+  () => import("@/components/_main/home/subscription"),
+  {
+    ssr: false,
+  }
+);
+
 // components
-import Hero from "src/components/_main/home/hero"; // Importing the Hero component.
-import WhyUs from 'src/components/_main/home/whyUs'; // Importing the WhyUs component.
-import TopBanners from "src/components/_main/home/topBanners"; // Importing the TopBanners component.
+import Hero from "@/components/_main/home/hero"; // Importing the Hero component.
+import WhyUs from "@/components/_main/home/whyUs"; // Importing the WhyUs component.
+import TopBanners from "@/components/_main/home/topBanners"; // Importing the TopBanners component.
 
 const IndexPage = () => {
   return (
---
+++ frontend/src/components/_main/home/categories/index.jsx
@@ -0,0 +1,18 @@
+"use client";
+// react
+import React from "react";
+import NextLink from "next/link";
+// mui
+import { Typography, Grid, Box, Stack, Paper, Button } from "@mui/material";
+import { IoIosArrowForward } from "react-icons/io";
+// component
+import CategoryCard from "@/components/cards/category";
+// api
+import * as api from "@/services";
+import { useQuery } from "react-query";
+
+const Categories = () => {
+  return <div>Categories</div>;
+};
+
+export default Categories;
