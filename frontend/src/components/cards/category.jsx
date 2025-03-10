e78b19123e71f5bcaf99d5a35d532ee87cb6730d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-03-10 11:25:36 +0100

    Commit

--- frontend/src/components/cards/category.jsx
+++ frontend/src/components/cards/category.jsx
@@ -17,7 +17,38 @@ const CategoriesCard = ({ ...props }) => {
   const { category, isLoading } = props;
   const baseUrl = "/products/";
 
-  return <div>CategoriesCard</div>;
+  return (
+    <Stack spacing={1} alignItems="center">
+      <Card
+        sx={{
+          borderRadius: "50%",
+          borderWidth: "3px !important",
+          transform: "scale(1.0)",
+          transition: "all 0.2s ease-in-out",
+          width: { xs: 90, md: 175 },
+          height: { xs: 90, md: 175 },
+          border: isLoading && "none !important",
+          "&:hover": {
+            color: "#000",
+            borderColor: (theme) => theme.palette.primary.main + "!important",
+            transform: "scale(1.05)",
+          },
+          "& .image-wrapper": {
+            position: "relative",
+            width: "100%",
+            img: {
+              borderRadius: "50%",
+            },
+            "&:after": {
+              content: `""`,
+              display: "block",
+              paddingBottom: "100%",
+            },
+          },
+        }}
+      ></Card>
+    </Stack>
+  );
 };
 
 export default CategoriesCard;
