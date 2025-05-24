7582dbc68b9d0383976ae188682e08f4e15cd866
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-24 20:15:15 +0100

    Commit

--- frontend/src/theme/index.jsx
+++ frontend/src/theme/index.jsx
@@ -2,21 +2,54 @@
 
 import PropTypes from "prop-types";
 import { CacheProvider } from "@emotion/react";
-import { ThemeProvider } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
 import React from "react";
 
 // custom theme
-import palette from './palette';
-import typography from './typography';
-import breakpoints from './breakpoints';
-import shape from './shape';
-import shadows, { customShadows } from './shadows';
-import componentsOverride from './overrides';
+import palette from "./palette";
+import typography from "./typography";
+import breakpoints from "./breakpoints";
+import shape from "./shape";
+import shadows, { customShadows } from "./shadows";
+import componentsOverride from "./overrides";
 
+ThemeRegistry.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const Localization = (lang) => {
+  switch (lang) {
+    case "ar":
+      return "arEG";
+    case "fr":
+      return "frFR";
+    case "en":
+      return "enUS";
+
+    default:
+      return "frFR";
+  }
+};
 const ThemeRegistry = ({ children }) => {
+  const { themeMode } = useSelector((state) => state.settings);
+
+  const customTheme = () => {
+    createTheme({
+      palette:
+        themeMode === "dark"
+          ? { ...palette.dark, mode: "dark" }
+          : { ...palette.light, mode: "light" },
+          direction: dir,
+    });
+  };
   return (
     <CacheProvider value={styleCache}>
-      <ThemeProvider theme={{ ...customTheme(), components:componentsOverride() }}></ThemeProvider>
+      <ThemeProvider
+        theme={{
+          ...customTheme(),
+          components: componentsOverride(customTheme()),
+        }}
+      ></ThemeProvider>
     </CacheProvider>
   );
 };
