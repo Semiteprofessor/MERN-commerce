891541b24fef6b5cb6002aa610f871e1c07c35df
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-15 17:03:42 +0100

    Commit

--- backend/controllers/category.ctl.js
+++ backend/controllers/category.ctl.js
@@ -238,3 +238,33 @@ const getSubCategoriesSlugs = async (req, res) => {
     res.status(400).json({ success: false, message: error.message });
   }
 };
+
+const getCategoryNameBySlug = async (req, res) => {
+  try {
+    const category = await Categories.findOne({
+      slug: req.params.slug,
+    }).select(["name", "slug"]);
+
+    res.status(201).json({
+      success: true,
+      data: category,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = {
+  createCategory,
+  getCategories,
+  getAllHeaderCategories,
+  getCategoryBySlug,
+  updateCategoryBySlug,
+  deleteCategoryBySlug,
+  getCategoriesSlugs,
+  getSubCategoriesSlugs,
+  getCategoryByAdmin,
+  getCategoryNameBySlug,
+  getAllCategories,
+  getCategoriesByAdmin,
+};
