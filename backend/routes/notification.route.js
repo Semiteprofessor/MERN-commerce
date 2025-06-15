6f7c2e21bb00a60530b97008ef6c8068ec67c357
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-15 15:57:33 +0100

    Commit

---
+++ backend/routes/notification.route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const notificationRoutes = require("../controllers/notification.ctl");
+// Import verifyToken function
+const verifyToken = require("../config/jwt");
+
+//admin routes
+router.get(
+  "/admin/notifications",
+  verifyToken,
+  notificationRoutes.getNotifications
+);
+router.post(
+  "/admin/notifications",
+  verifyToken,
+  notificationRoutes.createNotification
+);
+
+module.exports = router;
