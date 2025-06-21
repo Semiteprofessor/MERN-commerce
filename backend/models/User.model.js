bc19f5d9803c9b753127fa98ee0b8cc737accd80
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-21 21:21:30 +0100

    Commit

---
+++ backend/models/User.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, 'Please enter your firstName'],
+        },
+        lastName: {
+            type: String,
+            required: [true, 'Please enter lastName']
+        },
+        email: {
+            type: String,
+            required: [true, 'Please enter email address']
+        }
+    }
+)
