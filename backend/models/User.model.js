12625df48e2d727c11f948e7f3104523033aa8ad
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-21 21:32:36 +0100

    Commit

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const UserSchema = new mongoose.Schema(
-    {
-        firstName: {
-            type: String,
-            required: [true, 'Please enter your firstName'],
-        },
-        lastName: {
-            type: String,
-            required: [true, 'Please enter lastName']
-        },
-        email: {
-            type: String,
-            required: [true, 'Please enter email address']
-        }
-    }
-)
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Please enter your firstName"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please enter lastName"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter email address"],
+    unique: true,
+  },
+});
