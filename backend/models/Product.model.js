683c2b0d96dfffe598e5fc06e15b2ae87a22b058
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-21 21:41:55 +0100

    Commit

---
+++ backend/models/Product.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  code: {
+    type: String,
+  },
+  status: {
+    type: String,
+  },
+  isFeatured: {
+    type: Boolean,
+  },
+  brand: {
+    type: mongoose.Types.ObjectId,
+    ref: "Brand",
+  },
+  likes: {
+    type: Number,
+  },
+  description: {
+    type: String,
+  },
+  metaTitle: {
+    type: String,
+  },
+  metaDescription: {
+    type: String,
+  },
+  slug: {
+    type: String,
+    unique: true,
+  },
+  category: {
+    type: mongoose.Types.ObjectId,
+    ref: "Category",
+    required: [true, "please provide a category id"],
+  },
+  subCategory: {
+    type: mongoose.Types.ObjectId,
+    ref: "SubCategory",
+    required: [true, "please provide a sub category id"],
+  },
+});
