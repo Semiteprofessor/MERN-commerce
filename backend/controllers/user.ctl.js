e2a82eb8e704cb328dab66b600fc27ff4f5c84fd
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-22 15:30:32 +0100

    Commit

--- backend/controllers/user.ctl.js
+++ backend/controllers/user.ctl.js
@@ -57,3 +57,36 @@ const getUserByAdmin = async (req, res) => {
     return res.status(400).json({ success: false, message: error.message });
   }
 };
+
+const updateUser = async (req, res) => {
+  const user = await getUser(req, res);
+
+  const uid = user._id.toString();
+
+  try {
+    const data = await req.body;
+    const profile = await User.findByIdAndUpdate(
+      uid,
+      { ...data },
+      {
+        new: true,
+        runValidators: true,
+      }
+    ).select("-password");
+
+    if (!profile) {
+      return res.status(404).json({
+        success: false,
+        message: "User Not Found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: profile,
+    });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
+};
+  
