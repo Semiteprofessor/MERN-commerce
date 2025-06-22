d48a9589c829588ff4a24657c87f5798693bc3ec
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-22 15:26:57 +0100

    Commit

---
+++ backend/controllers/subCategory.ctl.js
@@ -0,0 +1,30 @@
+const SubCategories = require("../models/SubCategory.model");
+const Category = require("../models/Category.model");
+const getBlurDataURL = require("../config/getBlurDataURL");
+const { singleFileDelete } = require("../config/uploader");
+const createSubCategory = async (req, res) => {
+  try {
+    const { cover, ...others } = req.body;
+    // Validate if the 'blurDataURL' property exists in the logo object
+
+    // If blurDataURL is not provided, generate it using the 'getBlurDataURL' function
+    const blurDataURL = await getBlurDataURL(cover.url);
+
+    const category = await SubCategories.create({
+      ...others,
+      cover: {
+        ...cover,
+        blurDataURL,
+      },
+    });
+    await Category.findByIdAndUpdate(others.parentCategory, {
+      $addToSet: {
+        subCategories: category._id,
+      },
+    });
+
+    res.status(201).json({ success: true, message: "SubCategory Created" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
