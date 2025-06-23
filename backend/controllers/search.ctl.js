6c15cdeddd231952ed6095385519034b3588a402
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-23 15:16:31 +0100

    Commit

---
+++ backend/controllers/search.ctl.js
@@ -0,0 +1,75 @@
+const Products = require("../models/Product.model");
+const Shop = require("../models/Shop.model");
+const Category = require("../models/Category.model");
+const Brand = require("../models/Brand.model");
+const SubCategory = require("../models/SubCategory.model");
+
+const Search = async (req, res) => {
+  try {
+    const { query, shop, subCategory, category } = req.body;
+    const currenctShop = shop ? await Shop.findById(shop).select(["_id"]) : "";
+    const catId = category
+      ? await Category.findById(category).select(["_id"])
+      : "";
+    const subCatId = subCategory
+      ? await SubCategory.findById(subCategory).select(["_id"])
+      : "";
+    console.log(subCatId);
+    const products = await Products.aggregate([
+      {
+        $match: {
+          name: { $regex: query || "", $options: "i" },
+          ...(currenctShop && {
+            shop: currenctShop._id,
+          }),
+          ...(catId && {
+            category: catId._id,
+          }),
+          ...(catId &&
+            subCatId && {
+              subCategory: subCatId._id,
+            }),
+
+          status: { $ne: "disabled" },
+        },
+      },
+      {
+        $lookup: {
+          from: "categories", // Assuming 'categories' is the name of your Category model collection
+          localField: "category", // Field in the Products model
+          foreignField: "_id", // Field in the Category model
+          as: "categoryData",
+        },
+      },
+      {
+        $addFields: {
+          category: { $arrayElemAt: ["$categoryData.name", 0] }, // Extracting the title from the categoryData array
+
+          image: { $arrayElemAt: ["$images", 0] },
+        },
+      },
+
+      {
+        $project: {
+          image: { url: "$image.url", blurDataURL: "$image.blurDataURL" },
+          name: 1,
+          priceSale: 1,
+          slug: 1,
+          _id: 1,
+          category: 1, // Including the category field with only the title
+        },
+      },
+
+      {
+        $limit: 10,
+      },
+    ]);
+
+    return res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
+};
