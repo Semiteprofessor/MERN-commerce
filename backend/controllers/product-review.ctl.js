7d871b730b9cc18d6db98870ad645cdda684c904
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-24 22:46:11 +0100

    Commit

---
+++ backend/controllers/product-review.ctl.js
@@ -0,0 +1,53 @@
+const ProductReview = require("../models/ProductReview");
+const Products = require("../models/Product");
+const Users = require("../models/User");
+const { getUser } = require("../config/getUser");
+const Orders = require("../models/Order");
+const blurDataUrl = require("../config/getBlurDataURL");
+const getProductReviewsbyPid = async (req, res) => {
+  try {
+    // Populate the product
+    const pid = req.params.pid;
+    // Get reviews for the populated product
+    const reviews = await ProductReview.find({
+      product: pid,
+    })
+      .sort({
+        createdAt: -1,
+      })
+      .populate({
+        path: "user",
+        select: ["firstName", "lastName", "cover", "orders"],
+      });
+    const product = await Products.findById(pid).select(["slug"]);
+
+    const reviewsSummery = await Products.aggregate([
+      {
+        $match: { slug: product.slug },
+      },
+
+      {
+        $lookup: {
+          from: "productreviews",
+          localField: "_id",
+          foreignField: "product",
+          as: "productreviews",
+        },
+      },
+      {
+        $unwind: "$productreviews",
+      },
+
+      {
+        $group: {
+          _id: "$productreviews.rating",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    return res.status(201).json({ success: true, reviewsSummery, reviews });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
+};
