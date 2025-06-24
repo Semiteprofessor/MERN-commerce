9631271db87e28d8fcfc2a69fa5ce75559c63f57
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-24 15:04:56 +0100

    Commit

--- backend/controllers/product.ctl.js
+++ backend/controllers/product.ctl.js
@@ -305,3 +305,103 @@ const getProductsByCategory = async (req, res) => {
     });
   }
 };
+
+const getProductsByCompaign = async (req, res) => {
+  try {
+    const query = req.query; // Extract query params from request
+
+    var newQuery = { ...query };
+    delete newQuery.page;
+    delete newQuery.limit;
+    delete newQuery.name;
+    delete newQuery.date;
+    delete newQuery.price;
+    delete newQuery.top;
+    delete newQuery.rate;
+    for (const [key, value] of Object.entries(newQuery)) {
+      newQuery = { ...newQuery, [key]: value.split("_") };
+    }
+    const compaign = await Compaign.findOne({
+      slug: req.params.slug,
+    });
+    const skip = Number(query.limit) || 12;
+    const totalProducts = await Product.countDocuments({
+      _id: { $in: compaign.products },
+
+      status: { $ne: "disabled" },
+    }).select([""]);
+
+    const products = await Product.aggregate([
+      {
+        $lookup: {
+          from: "productreviews",
+          localField: "reviews",
+          foreignField: "_id",
+          as: "reviews",
+        },
+      },
+      {
+        $addFields: {
+          averageRating: { $avg: "$reviews.rating" },
+          image: { $arrayElemAt: ["$images", 0] },
+        },
+      },
+
+      {
+        $match: {
+          _id: { $in: compaign.products },
+          status: { $ne: "disabled" },
+        },
+      },
+      {
+        $project: {
+          image: { url: "$image.url", blurDataURL: "$image.blurDataURL" },
+          name: 1,
+          available: 1,
+          slug: 1,
+          colors: 1,
+          discount: 1,
+          likes: 1,
+          priceSale: 1,
+          available: 1,
+          price: 1,
+          averageRating: 1,
+          vendor: 1,
+          shop: 1,
+          createdAt: 1,
+        },
+      },
+      {
+        $sort: {
+          ...((query.date && { createdAt: Number(query.date) }) ||
+            (query.price && {
+              priceSale: Number(query.price),
+            }) ||
+            (query.name && { name: Number(query.name) }) ||
+            (query.top && { averageRating: Number(query.top) }) || {
+              averageRating: -1,
+            }),
+        },
+      },
+      {
+        $skip: Number(skip * parseInt(query.page ? query.page[0] - 1 : 0)),
+      },
+      {
+        $limit: Number(skip),
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      data: products,
+      total: totalProducts,
+      count: Math.ceil(totalProducts / skip),
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
