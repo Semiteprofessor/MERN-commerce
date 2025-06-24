df4bbc7641e81cb468713282ffe78ce8bdd4acd5
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-24 22:44:07 +0100

    Commit

--- backend/controllers/payment.js
+++ backend/controllers/payment.js
@@ -40,157 +40,157 @@ const getPaymentsByAdmin = async (req, res) => {
 
 // Create payment
 const createPayment = async (req, res) => {
-    try {
-      const newPayment = await Payment.create(req.body);
-      res
-        .status(201)
-        .json({ success: true, message: 'Payment created', data: newPayment });
-    } catch (error) {
-      res.status(400).json({ success: false, message: error.message });
+  try {
+    const newPayment = await Payment.create(req.body);
+    res
+      .status(201)
+      .json({ success: true, message: "Payment created", data: newPayment });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+// update payment
+const updatePayment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    // Find the existing payment
+    const existingPayment = await Payment.findById(id);
+
+    if (!existingPayment) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Payment not found" });
     }
-  };
-  
-  // update payment
-  const updatePayment = async (req, res) => {
-    try {
-      const { id } = req.params;
-      // Find the existing payment
-      const existingPayment = await Payment.findById(id);
-  
-      if (!existingPayment) {
-        return res
-          .status(404)
-          .json({ success: false, message: 'Payment not found' });
+
+    const updatedPayment = await Payment.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      {
+        new: true,
       }
-  
-      const updatedPayment = await Payment.findByIdAndUpdate(
-        id,
-        { ...req.body },
-        {
-          new: true,
-        }
-      );
-  
-      if (!updatedPayment) {
-        return res
-          .status(404)
-          .json({ success: false, message: 'Payment not found' });
-      }
-  
-      res.status(200).json({
-        success: true,
-        message: 'Payment updated',
-        data: updatedPayment,
-      });
-    } catch (error) {
-      res.status(400).json({ success: false, message: error.message });
+    );
+
+    if (!updatedPayment) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Payment not found" });
     }
-  };
-  
+
+    res.status(200).json({
+      success: true,
+      message: "Payment updated",
+      data: updatedPayment,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
 // Delete payment
 const deletePayment = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const deletedPayment = await Payment.findByIdAndDelete(id);
-      if (!deletedPayment) {
-        return res
-          .status(404)
-          .json({ success: false, message: 'Payment not found' });
-      }
-      res.status(200).json({ success: true, message: 'Payment deleted' });
-    } catch (error) {
-      res.status(400).json({ success: false, message: error.message });
+  try {
+    const { id } = req.params;
+    const deletedPayment = await Payment.findByIdAndDelete(id);
+    if (!deletedPayment) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Payment not found" });
     }
-  };
+    res.status(200).json({ success: true, message: "Payment deleted" });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
-  const getPaymentsByVender = async (req, res) => {
-    try {
-      const vendor = await getVendor(req, res);
-      if (!vendor) {
-        return res.status(400).json({ success: false, message: 'Not Allowed' });
-      }
-      const { limit, page = 1 } = req.query;
-  
-      const skip = parseInt(limit) || 8;
-      const totalPayments = await Payment.find().countDocuments();
-      const payments = await Payment.find()
-        .skip(skip * (parseInt(page) - 1 || 0))
-        .limit(skip)
-        .sort({ createdAt: -1 });
-  
-      res.status(200).json({
-        success: true,
-        data: payments,
-        count: Math.ceil(totalPayments / skip),
-      });
-    } catch (error) {
-      res.status(400).json({ success: false, message: error.message });
+const getPaymentsByVender = async (req, res) => {
+  try {
+    const vendor = await getVendor(req, res);
+    if (!vendor) {
+      return res.status(400).json({ success: false, message: "Not Allowed" });
     }
-  };
+    const { limit, page = 1 } = req.query;
 
-  const getPaymentDetailsById = async (req, res) => {
-    try {
-      const { pid } = req.params;
-      const { page = 1, limit = 10 } = req.query; // Set default values for page and limit
+    const skip = parseInt(limit) || 8;
+    const totalPayments = await Payment.find().countDocuments();
+    const payments = await Payment.find()
+      .skip(skip * (parseInt(page) - 1 || 0))
+      .limit(skip)
+      .sort({ createdAt: -1 });
 
-      const payment = await Payment.findOne({
-        _id: pid,
+    res.status(200).json({
+      success: true,
+      data: payments,
+      count: Math.ceil(totalPayments / skip),
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+const getPaymentDetailsById = async (req, res) => {
+  try {
+    const { pid } = req.params;
+    const { page = 1, limit = 10 } = req.query; // Set default values for page and limit
+
+    const payment = await Payment.findOne({
+      _id: pid,
+    })
+      .populate({
+        path: "shop",
+        select: [
+          "title",
+          "cover",
+          "logo",
+          "approved",
+          "approvedAt",
+          "description",
+          "status",
+          "phone",
+          "address",
+        ],
       })
-        .populate({
-          path: "shop",
-          select: [
-            "title",
-            "cover",
-            "logo",
-            "approved",
-            "approvedAt",
-            "description",
-            "status",
-            "phone",
-            "address",
-          ],
-        })
-        .populate({
-          path: "orders",
-          select: [
-            "items",
-            "createdAt",
-            "total",
-            "status",
-            "paymentMethod",
-            "user",
-          ],
-        });
-
-      const orders = payment.orders || []; // Handle case where 'orders' might be empty
-
-      // Pagination logic
-      const totalOrders = orders.length;
-      const start = (parseInt(page) - 1) * parseInt(limit);
-      const end = Math.min(start + parseInt(limit), totalOrders);
-      const paginatedOrders = orders.slice(start, end);
-      const { totalCommission, totalIncome, status, paidAt, date } = payment;
-      res.status(200).json({
-        success: true,
-        payment: {
-          totalCommission,
-          totalIncome,
-          status,
-          paidAt,
-          date,
-        },
-        shop: payment.shop,
-        data: {
-          data: paginatedOrders,
-          total: totalOrders,
-          count: Math.ceil(totalOrders / parseInt(limit)),
-        },
+      .populate({
+        path: "orders",
+        select: [
+          "items",
+          "createdAt",
+          "total",
+          "status",
+          "paymentMethod",
+          "user",
+        ],
       });
-    } catch (error) {
-      res.status(400).json({ success: false, message: error.message });
-    }
-  };
-  
+
+    const orders = payment.orders || []; // Handle case where 'orders' might be empty
+
+    // Pagination logic
+    const totalOrders = orders.length;
+    const start = (parseInt(page) - 1) * parseInt(limit);
+    const end = Math.min(start + parseInt(limit), totalOrders);
+    const paginatedOrders = orders.slice(start, end);
+    const { totalCommission, totalIncome, status, paidAt, date } = payment;
+    res.status(200).json({
+      success: true,
+      payment: {
+        totalCommission,
+        totalIncome,
+        status,
+        paidAt,
+        date,
+      },
+      shop: payment.shop,
+      data: {
+        data: paginatedOrders,
+        total: totalOrders,
+        count: Math.ceil(totalOrders / parseInt(limit)),
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
 const updatePaymentStatus = async (req, res) => {
   try {
     const { pid, sid } = req.params;
@@ -302,4 +302,58 @@ const lastMonthOrders = async (shopId) => {
 
   const lastMonthTotal = result[0] || null;
   return lastMonthTotal;
+};
+
+const thisMonthOrders = async (shopId) => {
+  const today = new Date();
+  const firstDayThisMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+  const tomorrow = new Date(today.getFullYear(), today.getMonth() + 1, 1); // Start of next month
+  const pipeline = [
+    {
+      $match: {
+        "items.shop": shopId,
+        status: "delivered",
+        createdAt: {
+          $gte: firstDayThisMonth,
+          $lt: tomorrow,
+        },
+      },
+    },
+    {
+      $project: {
+        // Existing projection fields (if any)
+        month: { $month: "$createdAt" },
+        year: { $year: "$createdAt" },
+        income: {
+          $sum: {
+            $map: {
+              input: {
+                $filter: {
+                  input: "$items",
+                  as: "item",
+                  cond: { $eq: ["$$item.shop", shopId] },
+                },
+              },
+              as: "item",
+              in: { $multiply: ["$$item.quantity", "$$item.priceSale"] }, // Calculate income per item
+            },
+          },
+        },
+        orderId: "$_id",
+      },
+    },
+    {
+      $group: {
+        _id: {
+          month: "$month",
+          year: "$year",
+        },
+        orders: { $push: "$$ROOT" },
+        totalIncome: { $sum: "$income" },
+      },
+    },
+  ];
+  const result = await Orders.aggregate(pipeline);
+  const thisMonthTotal = result[0] || null;
+  return thisMonthTotal;
 };
