63d906f3f6bc31b2ea84094ecf1d8f24f26d8186
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-25 15:49:18 +0100

    Commit

--- backend/routes/compaign.route.js
+++ backend/routes/compaign.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const compaign = require("../controllers/compaign");
+const compaign = require("../controllers/compaign.ctl");
 
 // Import verifyToken function
 const verifyToken = require("../config/jwt");
