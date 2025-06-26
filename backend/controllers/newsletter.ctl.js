b8e59c01cc74ad1d979f455ea175516dd36586f7
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-26 12:41:02 +0100

    Commit

---
+++ backend/controllers/newsletter.ctl.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Newsletter = require("../models/Newsletter");
+
+const getNewsletters = async (req, res) => {
+  try {
+    const skip = 10;
+    const NewsletterTotal = await Newsletter.find({}, null, {}).sort({
+      createdAt: -1,
+    });
+
+    const page = parseInt(req.query.page) || 1;
+
+    const data = await Newsletter.find({}, null, {
+      skip: skip * (page - 1),
+      limit: skip,
+    }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: data,
+      count: Math.ceil(NewsletterTotal.length / skip),
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
