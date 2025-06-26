d740980cf0eb257b8846e590364eeb3c0e7a0d3e
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-26 12:12:55 +0100

    Commit

--- backend/controllers/coupon-code.ctl.js
+++ backend/controllers/coupon-code.ctl.js
@@ -133,3 +133,32 @@ const updatedCouponCodeByAdmin = async (req, res) => {
     return res.status(400).json({ success: false, message: error.message });
   }
 };
+
+const deleteCouponCodeByAdmin = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const getCouponCode = await CouponCode.findById(id);
+    if (!getCouponCode) {
+      return res
+        .status(404)
+        .json({ success: false, message: "CouponCode Not Found" });
+    }
+    await CouponCode.findByIdAndDelete(id);
+    return res.status(200).json({
+      success: true,
+      message: "CouponCode Deleted ",
+    });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = {
+  getCouponCodeByCode,
+  getCouponCodesByAdmin,
+  createCouponCodeByAdmin,
+  getOneCouponCodeByAdmin,
+  updatedCouponCodeByAdmin,
+  deleteCouponCodeByAdmin,
+  getCouponCodeById,
+};
