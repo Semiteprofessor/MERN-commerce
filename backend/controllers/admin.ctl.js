3b048ae3b731eabc46f7a933a8ccc534162fb232
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-27 11:40:35 +0100

    Commit

--- backend/controllers/admin.ctl.js
+++ backend/controllers/admin.ctl.js
@@ -71,3 +71,42 @@ const getOrdersByUid = async (req, res) => {
     return res.status(400).json({ success: false, message: error.message });
   }
 };
+
+const UpdateRoleByAdmin = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userToUpdate = await User.findById(id);
+
+    if (!userToUpdate) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User Not Found." });
+    }
+
+    // Check if the user to update is a super admin
+    if (userToUpdate.role === "super admin") {
+      return res.status(403).json({
+        success: false,
+        message: "Cannot Change The Role Of A Super Admin.",
+      });
+    }
+
+    // Toggle the user's role
+    const newRole = userToUpdate.role === "user" ? "admin" : "user";
+
+    // Update the user's role
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { role: newRole },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: `${updatedUser.firstName} Is Now ${newRole}.`,
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+module.exports = { getUsersByAdmin, getOrdersByUid, UpdateRoleByAdmin };
