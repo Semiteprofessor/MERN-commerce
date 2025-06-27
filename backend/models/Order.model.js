850ae2e5766be7060f1fb6aa9a0859c9b3860616
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-27 10:17:00 +0100

    Commit

--- backend/models/Order.model.js
+++ backend/models/Order.model.js
@@ -32,4 +32,63 @@ const OrderSchema = new mongoose.Schema({
   discount: {
     type: Number,
   },
+  currency: {
+    type: String,
+    required: [true, "currency is required."],
+  },
+  conversionRate: {
+    type: Number,
+    required: [true, "Conversion rate is required."],
+  },
+  status: {
+    type: String,
+    enum: ["pending", "on the way", "delivered", "canceled", "returned"],
+  },
+  items: {
+    type: Array,
+  },
+  note: {
+    type: String,
+  },
+  user: {
+    _id: {
+      type: mongoose.Types.ObjectId, // Use ObjectId type
+    },
+    firstName: {
+      type: String,
+      required: [true, "First name is required."],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required."],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone is required."],
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required."],
+    },
+    city: {
+      type: String,
+      required: [true, "City is required."],
+    },
+    zip: {
+      type: String,
+      required: [true, "Postal code is required."],
+    },
+    country: {
+      type: String,
+      required: [true, "Country is required."],
+    },
+    state: {
+      type: String,
+      required: [true, "State is required."],
+    },
+  },
 });
