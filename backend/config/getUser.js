eeed79b5ff7569dce2d2a9a73cf5f0d89f1ae48b
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-27 11:05:29 +0100

    Commit

---
+++ backend/config/getUser.js
@@ -0,0 +1,30 @@
+const Users = require("../models/User");
+const Shop = require("../models/Shop");
+
+exports.getUser = async (req, res, requireVerify) => {
+  if (!req.user) {
+    return res
+      .status(401)
+      .json({ success: false, message: "You Must Be Logged In." });
+  }
+
+  try {
+    const user = await Users.findById(req.user._id);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User Not Found." });
+    }
+    if (!requireVerify && !user.isVerified) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User Email Is Not Verified." });
+    }
+
+    return user;
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error." });
+  }
+};
