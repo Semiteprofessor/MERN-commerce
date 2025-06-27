8375b4de653cb56cb47c273e75ee9acf02cd7041
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-27 11:06:41 +0100

    Commit

--- backend/config/getUser.js
+++ backend/config/getUser.js
@@ -55,3 +55,28 @@ exports.getAdmin = async (req, res) => {
   }
 };
 
+exports.getVendor = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "You Must Be Logged In." });
+    }
+
+    const user = await Users.findById(req.user._id);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User Not Found." });
+    }
+    if (!user.role.includes("vendor")) {
+      return { error: "Access Denied.", status: 401 };
+    }
+    return user;
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error." });
+  }
+};
+
