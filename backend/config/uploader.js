aabdd39ce9b02dc64f9d3bf12808a1f5f169dabd
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-27 11:12:11 +0100

    Commit

--- backend/config/uploader.js
+++ backend/config/uploader.js
@@ -33,3 +33,43 @@ exports.multiFileUploader = async (images) => {
   });
   return uploaded;
 };
+
+exports.singleFileUploader = async (image) => {
+  const result = await uploadOnCloudinary(image);
+  const uploaded = {
+    _id: result.public_id,
+    url: result.secure_url,
+  };
+  return uploaded;
+};
+
+exports.singleFileDelete = async (id) => {
+  const result = await deleteFromCloudinary(id);
+  return result;
+};
+
+// exports.multiFilesDelete = async images => {
+// 	var imageUrlList = [];
+// 	for (var i = 0; i < images.length; i++) {
+// 		var localFilePath = images[i];
+
+// 		// Upload the local image to Cloudinary
+// 		// and get image url as response
+// 		var result = await deleteFromCloudinary(localFilePath._id);
+// 		imageUrlList.push(result);
+// 	}
+// 	return result;
+// };
+exports.multiFilesDelete = async (images) => {
+  const results = [];
+
+  for (var i = 0; i < images.length; i++) {
+    var image = images[i];
+
+    // Delete the image from Cloudinary
+    var result = await deleteFromCloudinary(image._id);
+    results.push(result);
+  }
+
+  return results;
+};
