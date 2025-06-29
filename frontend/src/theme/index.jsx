09a8a3284eee76679439e6dc8dd399037d38bb7e
Taiwo Olapade <semiteprofessor@gmail.com>
2025-06-29 17:45:45 +0100

    Commit

--- frontend/src/theme/index.jsx
+++ frontend/src/theme/index.jsx
@@ -5,6 +5,14 @@ import { CacheProvider } from "@emotion/react";
 import { ThemeProvider } from "@mui/material";
 import React from "react";
 
+// custom theme
+import palette from './palette';
+import typography from './typography';
+import breakpoints from './breakpoints';
+import shape from './shape';
+import shadows, { customShadows } from './shadows';
+import componentsOverride from './overrides';
+
 const ThemeRegistry = ({ children }) => {
   return (
     <CacheProvider value={styleCache}>
