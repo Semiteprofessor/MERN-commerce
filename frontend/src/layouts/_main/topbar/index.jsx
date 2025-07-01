036e3cc9fb925abccea4441a6e23027001de08ab
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-01 15:38:25 +0100

    Commit

--- frontend/src/layouts/_main/topbar/index.jsx
+++ frontend/src/layouts/_main/topbar/index.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { useTheme } from "@mui/material";
 import { Container, Toolbar } from "@mui/material";
+import NextLink from "next/link";
 import React from "react";
 
 const UserTopBar = () => {
@@ -18,7 +21,37 @@ const UserTopBar = () => {
           zIndex: 999,
           px: "0px !important",
         }}
-      ></Toolbar>
+      >
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <Link
+            component={NextLink}
+            href={"tel:+13866883295"}
+            sx={{
+              color: "text.primary",
+              fontSize: 14,
+              display: "flex",
+              alignItems: "center",
+              gap: 1,
+            }}
+          >
+            <MdOutlinePhone /> +1 386-688-3295
+          </Link>
+          <Divider orientation="vertical" flexItem />
+          <Link
+            component={NextLink}
+            href={"mailto:johndoe@yahoo.com"}
+            sx={{
+              color: "text.primary",
+              fontSize: 14,
+              display: "flex",
+              alignItems: "center",
+              gap: 1,
+            }}
+          >
+            <MdOutlineMail /> johndoe@yahoo.com
+          </Link>
+        </Stack>
+      </Toolbar>
     </Container>
   );
 };
