ea5fbb110743489c3456dd3572ed0fe1a12e691f
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-01 12:44:50 +0100

    Commit

---
+++ frontend/src/app/not-found.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Box, Button, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: 3,
+        textAlign: "center",
+      }}
+    >
+      <Typography variant="h4" color="text.primary">
+        <NotFoundIllustration />
+      </Typography>
+      <Typography variant="body1" color="inherit">
+        Something went wrong. It looks like the link is broken or the page was
+        removed.
+      </Typography>
+      <Box sx={{ display: "flex", gap: 2 }}>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={() => router.back()}
+        >
+          Go Back
+        </Button>
+        <Button variant="outlined" onClick={() => router.push("/")}>
+          Go To Home
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default NotFound;
