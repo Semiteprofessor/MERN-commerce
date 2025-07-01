d3916f08da4e1f2be4f3f814dda904ae00128138
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-01 21:57:30 +0100

    Commit

--- frontend/src/components/dialog/search/Search.jsx
+++ frontend/src/components/dialog/search/Search.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [focus, setFocus] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const [state, setState] = useState({
+    products: [],
+    selected: selectedProducts || [],
+    initialized: false,
+    category: "",
+    subCategory: "",
+    shop: "",
+  });
+
+  const onKeyDown = (e) => {
+    if (e.keyCode == "38" || e.keyCode == "40") {
+      setFocus(false);
+    }
+  };
+  return (
+    <>
+      <TextField
+        id="standard-basic"
+        variant="standard"
+        placeholder="Search products"
+        onFocus={() => setFocus(true)}
+        onKeyDown={onKeyDown}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+    </>
+  );
 };
 
 export default Search;
