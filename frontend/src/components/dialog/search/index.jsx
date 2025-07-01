27c3124b44b5bbb4292a6c1c89f3b829ce378144
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-01 18:55:04 +0100

    Commit

--- frontend/src/components/dialog/search/index.jsx
+++ frontend/src/components/dialog/search/index.jsx
@@ -1,14 +1,45 @@
-import { Stack } from "@mui/material";
-import React from "react";
+import { alpha, IconButton, Stack, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 const SimpleDialogDemo = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <Stack
         direction="row"
         alignItems="center"
-        justifyContent="space-between" onClick={handleClickOpen}
-      ></Stack>
+        justifyContent="space-between"
+        onClick={handleClickOpen}
+        sx={{
+          p: 1,
+          border: (theme) => `1px solid ${theme.palette.divider}`,
+          borderRadius: 10,
+          width: 300,
+          height: 56,
+          cursor: "pointer",
+        }}
+      >
+        <Typography>Search...</Typography>
+        <IconButton
+          onClick={handleClickOpen}
+          name="search"
+          color="primary"
+          sx={{
+            borderColor: "primary",
+            borderWidth: 1,
+            borderStyle: "solid",
+            bgcolor: (theme) => alpha(theme.palette.primary.main, 0.1),
+          }}
+        />
+      </Stack>
     </>
   );
 };
