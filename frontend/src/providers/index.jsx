79a5591e311acd522eeaf1e4e40721e64b31e009
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-02 11:50:27 +0100

    Commit

--- frontend/src/providers/index.jsx
+++ frontend/src/providers/index.jsx
@@ -1,8 +1,11 @@
+import ThemeRegistry from "@/theme";
 import { LinearProgress, Stack } from "@mui/material";
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { PersistGate } from "redux-persist/integration/react";
+
+import { reduxStore, persistor } from "src/redux";
 
 const Providers = (props) => {
   const [queryClient] = useState(
@@ -16,26 +19,29 @@ const Providers = (props) => {
       })
   );
   return (
-    <QueryClientProvider client={queryClient}>
-      <Toaster position={"top-center"} />
-      <PersistGate
-        loading={
-          <Stack
-            sx={{
-              position: "fixed",
-              top: "calc(50vh - 2px)",
-              width: "300px",
-              left: "calc(50vw - 150px",
-              zIndex: 11,
-            }}
-          >
-            <LinearProgress />
-          </Stack>
-        }
-      >
-        {props.children}
-      </PersistGate>
-    </QueryClientProvider>
+    <ThemeRegistry>
+      <QueryClientProvider client={queryClient}>
+        <Toaster position={"top-center"} />
+        <PersistGate
+          loading={
+            <Stack
+              sx={{
+                position: "fixed",
+                top: "calc(50vh - 2px)",
+                width: "300px",
+                left: "calc(50vw - 150px",
+                zIndex: 11,
+              }}
+            >
+              <LinearProgress />
+            </Stack>
+          }
+          persistor={persistor}
+        >
+          {props.children}
+        </PersistGate>
+      </QueryClientProvider>
+    </ThemeRegistry>
   );
 };
 
---
+++ frontend/src/theme/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ThemeRegistry = () => {
+  return <div>ThemeRegistry</div>;
+};
+
+export default ThemeRegistry;
