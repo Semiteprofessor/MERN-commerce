e4a9561c48522c100621e9327f9b4d105b1027f3
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-02 22:04:46 +0100

    Commit

---
+++ frontend/src/redux/slices/shops.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    shops = [];
+    isLoading = true
+}
+
+const slice = createSlice({
+    name: 'shops',
+    initialState,
+    reducers: {
+        setShops(state, action) {
+            state.shops = action.payload;
+            state.isLoading = false
+        }
+    }
+})
+
+export default slice.reducer;
+
+export const {setShops} = slice.actions
