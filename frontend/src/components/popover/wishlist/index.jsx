982ff3afa755a1823596da543bae6f14e17de583
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-04 19:02:18 +0100

    Commit

--- frontend/src/components/popover/wishlist/index.jsx
+++ frontend/src/components/popover/wishlist/index.jsx
@@ -1,7 +1,14 @@
-import { Stack } from "@mui/material";
+import { IconButton, Stack, Typography } from "@mui/material";
+import { useRouter } from "next-nprogress-bar";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const WishlistPopover = () => {
+  const router = useRouter();
+
+  const { wishlist } = useSelector(({ wishlist }) => wishlist);
+  const { isAuthenticated } = useSelector(({ user }) => user);
+
   return (
     <Stack
       direction="row"
@@ -15,10 +22,36 @@ const WishlistPopover = () => {
           router.push("/profile/wishlist");
         }
       }}
+      spacing={1}
     >
-      WishlistPopover
+      <IconButton
+        name="wishlist"
+        color="primary"
+        disableRipple
+        sx={() => {
+          if (!isAuthenticated) {
+            router.push("/auth/login");
+          } else {
+            router.push("/profile/wishlist");
+          }
+        }}
+      >
+        <IoMdHeartEmpty />
+      </IconButton>
+      <Stack>
+        <Typography variant="subtitle2" color="text.primary" mb={-0.6}>
+          Wishlist
+        </Typography>
+        <Typography>
+          {wishlist?.length || 0} {wishlist?.length > 1 ? "Items" : "Item"}
+        </Typography>
+      </Stack>
     </Stack>
   );
+};
+
+WishlistPopover.propTypes = {
+  isAuth: PropTypes.bool.isRequired,
 };
 
 export default WishlistPopover;
