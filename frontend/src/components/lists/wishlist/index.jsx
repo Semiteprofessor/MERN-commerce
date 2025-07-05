1ae7bfc0f7eec47ec503809eab6948b238e1160d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-05 12:40:55 +0100

    Commit

--- frontend/src/components/lists/wishlist/index.jsx
+++ frontend/src/components/lists/wishlist/index.jsx
@@ -64,80 +64,117 @@ const SkeletonComponent = () => {
 };
   
 export default function Wishlist({ ...props }) {
-    const { item, isLast, isUser } = props;
-    const router = useRouter();
-    const t = (v) => v;
-    // const { checkout } = useSelector((state) => state.product);
-    const dispatch = useDispatch();
-    const linkTo = `/products/${item.slug}`;
-    const [isLoading, setLoading] = useState(false);
-    const { mutate, isLoading: deleteLoading } = useMutation(api.updateWishlist, {
-      onSuccess: (data) => {
-        setLoading(false);
-        toast.success('Removed item');
-        dispatch(setWishlist(data.data));
-      },
-      onError: (err) => {
-        setLoading(false);
-        toast.error(t('common:errors.' + err.response.data.message));
-      }
+  const { item, isLast, isUser } = props;
+  const router = useRouter();
+  const t = (v) => v;
+  // const { checkout } = useSelector((state) => state.product);
+  const dispatch = useDispatch();
+  const linkTo = `/products/${item.slug}`;
+  const [isLoading, setLoading] = useState(false);
+  const { mutate, isLoading: deleteLoading } = useMutation(api.updateWishlist, {
+    onSuccess: (data) => {
+      setLoading(false);
+      toast.success("Removed item");
+      dispatch(setWishlist(data.data));
+    },
+    onError: (err) => {
+      setLoading(false);
+      toast.error(t("common:errors." + err.response.data.message));
+    },
+  });
+
+  const onRemove = () => {
+    mutate({
+      pid: item._id,
     });
-  
-    const onRemove = () => {
-      mutate({
-        pid: item._id
-      });
-    };
-    return (
-      <RootStyled>
-        <List disablePadding>
-          <ListItem>
+  };
+  return (
+    <RootStyled>
+      <List disablePadding>
+        <ListItem>
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+            sx={{ width: "100%" }}
+            className="main-stack"
+          >
             <Stack
               direction="row"
+              className={isUser && "inner-stack"}
+              sx={{ width: "100%" }}
               alignItems="center"
+              spacing={2}
               justifyContent="space-between"
-              sx={{ width: '100%' }}
-              className="main-stack"
             >
-              <Stack
-                direction="row"
-                className={isUser && 'inner-stack'}
-                sx={{ width: '100%' }}
-                alignItems="center"
-                spacing={2}
-                justifyContent="space-between"
-              >
-                <Stack direction="row" alignItems="center" spacing={2}>
-                  <ListItemAvatar>
-                    <Avatar
+              <Stack direction="row" alignItems="center" spacing={2}>
+                <ListItemAvatar>
+                  <Avatar
+                    onClick={() => router.push(linkTo)}
+                    alt={item.name}
+                    src={item.cover}
+                    className="list-item-avatar"
+                  />
+                </ListItemAvatar>
+                <ListItemText className="list-item-text">
+                  <div>
+                    <Typography
                       onClick={() => router.push(linkTo)}
-                      alt={item.name}
-                      src={item.cover}
-                      className="list-item-avatar"
+                      variant="subtitle1"
+                      color="text.primary"
+                      noWrap
+                      sx={{
+                        cursor: "pointer",
+                        transition: "ease-in-out 0.3s",
+                        ":hover": {
+                          color: "primary.main",
+                        },
+                      }}
+                    >
+                      {item.name.slice(0, 40)}
+                    </Typography>
+                    <Typography
+                      className="list-item-text-span"
+                      component="span"
+                      variant="body2"
+                      color="text.primary"
+                    >
+                      In Stock: {""}
+                      {item?.available} {""}Items
+                    </Typography>
+                  </div>
+                </ListItemText>
+              </Stack>
+
+              <Stack justifyContent={"space-around"} sx={{ float: "right" }}>
+                <Box>
+                  {deleteLoading ? (
+                    <Skeleton
+                      variant="rectangular"
+                      width={32}
+                      height={32}
+                      className="list-skeleton"
                     />
-                  </ListItemAvatar>
-                  <ListItemText className="list-item-text">
-                    <div>
-                      <Typography
-                        onClick={() => router.push(linkTo)}
-                        variant="subtitle1"
-                        color="text.primary"
-                        noWrap
-                        sx={{
-                          cursor: 'pointer',
-                          transition: 'ease-in-out 0.3s',
-                          ':hover': {
-                            color: 'primary.main'
-                          }
-                        }}
-                      >
-                        {item.name.slice(0, 40)}
-                      </Typography>
-                      <Typography className="list-item-text-span" component="span" variant="body2" color="text.primary">
-                        In Stock: {''}
-                        {item?.available} {''}Items
-                      </Typography>
-                    </div>
-                  </ListItemText>
-                </Stack>
-  
+                  ) : (
+                    <IconButton
+                      size="small"
+                      className="list-icon-btn"
+                      onClick={onRemove}
+                    >
+                      <DeleteOutlineRoundedIcon fontSize="small" />
+                    </IconButton>
+                  )}
+                </Box>
+              </Stack>
+            </Stack>
+          </Stack>
+        </ListItem>
+
+        {!isLast && <Divider component="li" sx={{ mx: 1 }} />}
+        {(isLoading ? Array.from(new Array(7)) : []).map(() => (
+          <SkeletonComponent key={Math.random()} />
+        ))}
+      </List>
+    </RootStyled>
+  );
+}
