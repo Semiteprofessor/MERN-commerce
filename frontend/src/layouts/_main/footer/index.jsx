5c4c114603b2edc34d5b2fd6ad0c755924445887
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-05 23:32:27 +0100

    Commit

--- frontend/src/layouts/_main/footer/index.jsx
+++ frontend/src/layouts/_main/footer/index.jsx
@@ -40,3 +40,46 @@ const SOCIAL_MEDIA_LINK = [
     icon: <FaLinkedinIn size={18} />,
   },
 ];
+
+const ADDRESS = [
+  {
+    name: "Ohio St. South Gate, CA 90280",
+    icon: <MdOutlineLocationOn />,
+  },
+  {
+    name: "johndoe@yahoo.com",
+    linkPath: "/",
+    icon: <FiMail fontSize={20} />,
+  },
+  {
+    name: "+1 386-688-3295",
+    linkPath: "/",
+    icon: <MdOutlineCall />,
+  },
+];
+
+const MAIN_LINKS = [
+  {
+    heading: "Resources",
+    listText1: "Contact us",
+    listLink1: "/contact",
+    listText2: "Products",
+    listLink2: "/products",
+    listText3: "Shops",
+    listLink3: "/shops",
+    listText4: "Compaigns",
+    listLink4: "/compaigns",
+  },
+  {
+    heading: "About us",
+    listText1: "About us",
+    listLink1: "/about",
+    listText2: "Privacy policy",
+    listLink2: "/privacy-policy",
+    listText3: "Term and conditions",
+    listLink3: "/terms-and-conditions",
+    listText4: "Refund return policy",
+    listLink4: "/refund-return-policy",
+  },
+];
+  
