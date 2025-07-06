b10aa74cbcde820dfe1b295e0d899e7bbf59b7ec
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-06 14:19:47 +0100

    Commit

--- frontend/src/components/mega-menu/MegaMenu.js
+++ frontend/src/components/mega-menu/MegaMenu.js
@@ -18,17 +18,75 @@ import {
   Skeleton,
 } from "@mui/material";
 // api
-import * as api from "src/services";
+import * as api from "@/services";
 import { useQuery } from "react-query";
 // redux
-import { setShops } from "src/redux/slices/shops";
+import { setShops } from "@/redux/slices/shops";
 import { useDispatch } from "react-redux";
 
 // ----------------------------------------------------------------------
 
 const ITEM_HEIGHT = 40;
 // ----------------------------------------------------------------------
+function ParentItem({ shop, isLast, isLoading }) {
+  const activeStyle = {
+    color: "primary.main",
+    bgcolor: (theme) =>
+      alpha(theme.palette.primary.main, theme.palette.action.hoverOpacity),
+  };
 
+  return (
+    <ListItem
+      href={`/shops/${shop?.slug}`}
+      component={RouterLink}
+      sx={{
+        padding: (theme) => theme.spacing(3.5, 2),
+        height: ITEM_HEIGHT,
+        cursor: "pointer",
+        color: "text.primary",
+        typography: "subtitle2",
+        textTransform: "capitalize",
+        justifyContent: "space-between",
+        transition: (theme) => theme.transitions.create("all"),
+        borderBottom: (theme) =>
+          `1px solid ${isLast ? "transparent" : theme.palette.divider}`,
+        "&:hover": activeStyle,
+      }}
+    >
+      <Stack direction="row" spacing={2} alignItems="center">
+        <Box
+          component="span"
+          sx={{
+            width: 40,
+            height: 40,
+            borderRadius: "50%",
+            position: "relative",
+            overflow: "hidden",
+            border: (theme) => `solid 1px ${theme.palette.divider}`,
+          }}
+        >
+          {isLoading ? (
+            <Skeleton variant="circular" width={40} height={40} />
+          ) : (
+            <Image
+              src={shop?.logo?.url}
+              placeholder="blur"
+              blurDataURL={shop?.logo?.blurDataURL}
+              alt={shop?.title}
+              layout="fill"
+              objectFit="cover"
+              size="30vw"
+            />
+          )}
+        </Box>
+        <Typography variant="body1" color="text.primary" fontWeight={500}>
+          {isLoading ? <Skeleton variant="text" width={120} /> : shop?.title}
+        </Typography>
+      </Stack>
+    </ListItem>
+  );
+}
+  
 const MegaMenu = () => {
   return <div>MegaMenu</div>;
 };
