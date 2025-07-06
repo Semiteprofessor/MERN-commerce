9302d3b617dbb19be788a2affbb9f9b7812d4710
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-06 12:52:25 +0100

    Commit

--- frontend/src/components/carousels/singleSlide/index.jsx
+++ frontend/src/components/carousels/singleSlide/index.jsx
@@ -146,7 +146,45 @@ const SingleSlideCarousel = ({ ...props }) => {
           </Typography>
         ) : (
           <AnimatePresence initial={false} custom={direction}>
-            Hellooo
+            <motion.div
+              style={{
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                overflow: "hidden",
+                top: 0,
+              }}
+              key={page}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.2 },
+              }}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragElastic={1}
+              onDragEnd={(e, { offset, velocity }) => {
+                const swipe = swipePower(offset.x, velocity.x);
+                if (swipe < -swipeConfidenceThreshold) {
+                  paginate(1);
+                } else if (swipe > swipeConfidenceThreshold) {
+                  paginate(-1);
+                }
+              }}
+            >
+              <CarouselItem
+                themeMode={themeMode}
+                item={data ? data[imageIndex] : null}
+                index={data ? data[imageIndex] : null}
+                activeStep={imageIndex}
+                isActive={imageIndex}
+                key={Math.random()}
+              />
+            </motion.div>
           </AnimatePresence>
         )}
       </Stack>
