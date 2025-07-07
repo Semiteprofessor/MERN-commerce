eb876f15db6f7d1418d5c0712588e5dc21f64809
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-07 12:52:56 +0100

    Commit

--- frontend/src/components/carousels/customPagination/index.jsx
+++ frontend/src/components/carousels/customPagination/index.jsx
@@ -43,7 +43,25 @@ const swipePower = (offset, velocity) => {
 };
 
 const ProductDetailsCarousel = ({ ...props }) => {
-  return <div>ProductDetailsCarousel</div>;
+    const { item } = props;
+  
+  return (
+    <div className="slide-wrapper">
+      {item && (
+        <BlurImage
+          priority
+          fill
+          objectFit="cover"
+          sizes="50%"
+          src={item?.url || item?.src}
+          alt="hero-carousel"
+          placeholder="blur"
+          blurDataURL={item.blurDataURL}
+        />
+      )}
+      <Box className="bg-overlay" />
+    </div>
+  );
 };
 
 // ----------------------------------------------------------------------
@@ -51,4 +69,53 @@ ProductDetailsCarousel.propTypes = {
   item: PropTypes.object.isRequired,
 };
 
-export default ProductDetailsCarousel;
+const CarouselAnimation =({...props}) => {
+    
+  const { product } = props;
+
+  const images = product?.images;
+
+  const [[page, direction], setPage] = useState([0, 0]);
+  const imageIndex = Math.abs(page % images?.length);
+
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
+  return (
+    <RootStyled>
+      <div className="carousel-wrap">
+        <AnimatePresence initial={false} custom={direction}>
+          <motion.div
+            className="motion-dev"
+            key={page}
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: { type: "spring", stiffness: 300, damping: 30 },
+              opacity: { duration: 0.2 },
+            }}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x);
+              if (swipe < -swipeConfidenceThreshold) {
+                paginate(1);
+              } else if (swipe > swipeConfidenceThreshold) {
+                paginate(-1);
+              }
+            }}
+          >
+            <ProductDetailsCarousel item={images[imageIndex]} />
+          </motion.div>
+        </AnimatePresence>
+      </div>
+    </RootStyled>
+  );
+
+}
+export default CarouselAnimation;
