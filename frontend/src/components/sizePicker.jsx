8a15bd3969f7a7a9f56b33f50c15e4cb135790a4
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-08 10:36:55 +0100

    Commit

--- frontend/src/components/sizePicker.jsx
+++ frontend/src/components/sizePicker.jsx
@@ -55,6 +55,63 @@ const SizePreview = ({ sizes, size, setSize, isDetail, loading }) => {
           </Button>
         </Zoom>
       )}
+      {loading
+        ? Array.from(new Array(4))
+        : sizes?.slice(sizeCount * 6, 6 * (sizeCount + 1)).map((v, i) => (
+            <React.Fragment key={Math.random()}>
+              {loading ? (
+                <Skeleton
+                  variant="rounded"
+                  width={25}
+                  height={24}
+                  sx={{ mr: 0.5 }}
+                />
+              ) : (
+                <Button
+                  size="small"
+                  variant={size === i ? "contained" : "outlined"}
+                  color={size === i ? "primary" : "inherit"}
+                  onClick={() => setSize(i)}
+                  sx={{
+                    minHeight: 24,
+                    minWidth: 25,
+                    height: "24px !important",
+                    px: 0.6,
+                    py: 0.2,
+                    borderWidth: 1,
+                    textTransform: "uppercase",
+                    fontSize: size === i ? 14 : 12,
+                    borderWidth: 0,
+                  }}
+                >
+                  {v}
+                </Button>
+              )}
+            </React.Fragment>
+          ))}
+      {!isDetail && sizes?.length > 6 && (
+        <Zoom in={6 * (sizeCount + 1) < sizes?.length}>
+          <Button
+            variant="outlined"
+            color="inherit"
+            onClick={() => {
+              if (6 * (sizeCount + 1) < sizes?.length) {
+                setSizeCount(sizeCount + 1);
+              }
+            }}
+            sx={{
+              minHeight: 24,
+              minWidth: 25,
+              borderWidth: 0,
+              height: "24px !important",
+              color: "text.primary !important",
+              p: 0.2,
+            }}
+          >
+            <MdKeyboardDoubleArrowRight size={20} />
+          </Button>
+        </Zoom>
+      )}
     </Stack>
   );
 };
