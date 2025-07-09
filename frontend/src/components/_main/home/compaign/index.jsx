ef3d0ec8b2d6dbf0ba400df7024eb00e9969c346
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-09 13:00:11 +0100

    Commit

--- frontend/src/components/_main/home/compaign/index.jsx
+++ frontend/src/components/_main/home/compaign/index.jsx
@@ -12,7 +12,49 @@ import * as api from "@/services";
 import { useQuery } from "react-query";
 
 const CompaignsComponent = () => {
-  return <div>CompaignsComponent</div>;
+  const { data, isLoading } = useQuery(["get-home-compaign-all"], () =>
+    api.getHomeCompaigns("?limit=4")
+  );
+
+  return (
+    <Paper elevation={0}>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        textAlign={{ xs: "center", md: "left" }}
+        alignItems="center"
+      >
+        <Box width="100%">
+          <Typography variant="h2" color="text.primary" mt={{ xs: 4, md: 8 }}>
+            All Compaigns
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            mb={{ xs: 3, md: 5 }}
+          >
+            All of Ours Compaigns{" "}
+          </Typography>
+        </Box>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          sx={{
+            borderRadius: 6,
+            display: { xs: "none", md: "flex" },
+            minWidth: 130,
+            px: 1,
+          }}
+          endIcon={<IoIosArrowForward />}
+          component={NextLink}
+          href={`/compaigns`}
+        >
+          View More
+        </Button>
+      </Stack>
+    </Paper>
+  );
 };
 
 export default CompaignsComponent;
