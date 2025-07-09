d67233e86a3e8a6af80872c97a048eb8757ddb26
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-09 12:13:50 +0100

    Commit

--- frontend/src/components/carousels/gridSlider/index.jsx
+++ frontend/src/components/carousels/gridSlider/index.jsx
@@ -139,7 +139,41 @@ const ProductsCarousel = ({ ...props }) => {
             pr: { md: `24px !important`, xs: `0px !important` },
           },
         }}
-      ></Paper>
+      >
+        <AnimatePresence initial={false} custom={direction}>
+          <motion.div
+            className="motion-dev"
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              overflow: "hidden",
+              top: 0,
+            }}
+            key={page}
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: { type: "spring", stiffness: 300, damping: 30 },
+              opacity: { duration: 0.2 },
+            }}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x);
+              if (swipe < -swipeConfidenceThreshold) {
+                paginate(1);
+              } else if (swipe > swipeConfidenceThreshold) {
+                paginate(-1);
+              }
+            }}
+          ></motion.div>
+        </AnimatePresence>
+      </Paper>
     </Paper>
   );
 };
