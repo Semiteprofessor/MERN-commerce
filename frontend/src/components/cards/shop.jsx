d9947576cf4d0bb52dc8b3b939cbd493305a126b
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-10 20:55:52 +0100

    Commit

--- frontend/src/components/cards/shop.jsx
+++ frontend/src/components/cards/shop.jsx
@@ -129,6 +129,156 @@ const ShopCard = ({ ...props }) => {
           </Box>
         )}
       </Box>
+      <CardContent>
+        <Box sx={{ mt: -7 }}>
+          {isLoading ? (
+            <Skeleton
+              variant="circular"
+              sx={{
+                height: 70,
+                width: 70,
+                mx: "auto",
+              }}
+            />
+          ) : (
+            <Box
+              sx={{
+                position: "relative",
+                height: 70,
+                width: 70,
+                minWidth: 70,
+                borderRadius: "50%",
+                bgcolor: "background.paper",
+                mx: "auto",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Box
+                sx={{
+                  position: "relative",
+                  border: (theme) => `1px solid ${theme.palette.divider}`,
+                  height: 64,
+                  width: 64,
+                  minWidth: 64,
+                  borderRadius: "50%",
+                  img: {
+                    borderRadius: "50%",
+                  },
+                  "&:after": {
+                    content: `""`,
+                    display: "block",
+                    paddingBottom: "100%",
+                  },
+                }}
+              >
+                <Image
+                  alt="shop"
+                  src={shop?.logo?.url}
+                  placeholder="blur"
+                  blurDataURL={shop?.logo?.blurDataURL}
+                  layout="fill"
+                  objectFit="cover"
+                  static
+                  draggable="false"
+                  quality={5}
+                  sizes={"50vw"}
+                />
+              </Box>
+            </Box>
+          )}
+        </Box>
+        <Stack spacing={1}>
+          <Typography
+            {...(!isLoading && {
+              component: Link,
+              href: baseUrl + shop?.slug,
+            })}
+            color="text.primary"
+            variant="h6"
+            textAlign="center"
+            lineHeight={0.5}
+            sx={{ textTransform: "capitalize", pt: 2 }}
+          >
+            {isLoading ? (
+              <Skeleton variant="text" width={100} sx={{ mx: "auto" }} />
+            ) : (
+              shop?.title
+            )}
+          </Typography>
+          <Typography color="text.secondary" variant="body1" textAlign="center">
+            {isLoading ? (
+              <Skeleton variant="text" width={"100%"} />
+            ) : (
+              shop.description
+            )}
+          </Typography>
+        </Stack>
+        <Stack direction="row" spacing={1.5} justifyContent="center" mt={1}>
+          {isLoading ? (
+            <Skeleton
+              variant="rectanguar"
+              width={121}
+              height={32}
+              sx={{
+                borderRadius: "24px",
+              }}
+            />
+          ) : (
+            <Button
+              variant="outlined"
+              color="primary"
+              size="small"
+              type="button"
+              onClick={() => router.push(baseUrl + shop?.slug)}
+              startIcon={<AiOutlineShop />}
+              sx={{
+                borderRadius: 6,
+                fontWeight: 400,
+                whiteSpace: "nowrap",
+                px: 2,
+              }}
+            >
+              View Store
+            </Button>
+          )}
+          {isLoading || loading ? (
+            <Skeleton
+              variant="rectanguar"
+              width={92}
+              height={32}
+              sx={{
+                borderRadius: "24px",
+              }}
+            />
+          ) : (
+            <Button
+              onClick={() => {
+                setLoading(true);
+                mutate(shop._id);
+              }}
+              variant={
+                followingShops.filter((v) => v === shop._id).length
+                  ? "contained"
+                  : "outlined"
+              }
+              size="small"
+              color="secondary"
+              startIcon={<FaRegUser size={16} />}
+              sx={{
+                borderRadius: 6,
+                fontWeight: 400,
+                px: 2,
+              }}
+            >
+              {followingShops.filter((v) => v === shop._id).length
+                ? "Unfollow"
+                : "Follow"}
+            </Button>
+          )}
+        </Stack>
+      </CardContent>
     </Card>
   );
 };
