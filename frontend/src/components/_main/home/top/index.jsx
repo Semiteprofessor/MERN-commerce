07150a64bbcd8f5bdf239356dc2ff5ace17ad0ea
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-10 20:13:33 +0100

    Commit

--- frontend/src/components/_main/home/top/index.jsx
+++ frontend/src/components/_main/home/top/index.jsx
@@ -5,10 +5,10 @@ import NextLink from "next/link";
 // mui
 import { Typography, Box, Button, Stack } from "@mui/material";
 // api
-import * as api from "src/services";
+import * as api from "@/services";
 import { useQuery } from "react-query";
 // components
-import ProductsCarousel from "src/components/carousels/gridSlider";
+import ProductsCarousel from "@/components/carousels/gridSlider";
 // icons
 import { IoIosArrowForward } from "react-icons/io";
 
@@ -55,6 +55,29 @@ const Index = () => {
           View More
         </Button>
       </Stack>
+      {!isLoading && !Boolean(data?.data.length) ? (
+        <Typography variant="h3" color="error.main" textAlign="center">
+          Products not found
+        </Typography>
+      ) : (
+        <ProductsCarousel data={data?.data || []} isLoading={isLoading} />
+      )}
+      <Button
+        variant="text"
+        color="primary"
+        size="small"
+        sx={{
+          borderRadius: 6,
+          mx: "auto",
+          display: { md: "none", xs: "flex" },
+          maxWidth: "120px",
+        }}
+        endIcon={<IoIosArrowForward />}
+        component={NextLink}
+        href={`/products?top=1`}
+      >
+        View More
+      </Button>
     </Box>
   );
 };
