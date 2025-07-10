6231819240d75d04d82a8e03db57dc1d361b1ce4
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-10 20:07:49 +0100

    Commit

--- frontend/src/components/_main/home/banner/index.jsx
+++ frontend/src/components/_main/home/banner/index.jsx
@@ -37,6 +37,36 @@ const Banner = () => {
           sizes="700px"
           objectFit="cover"
         />
+        <Container>
+          <Grid>
+            <Grid>
+              <Stack>
+                <Typography sx={{ zIndex: 11 }} variant="h2" fontWeight={900}>
+                  UK Premier Store for Wrist Watches
+                </Typography><Typography sx={{ zIndex: 11 }} variant="body1" color="text.success">
+                  Welcome to our world of horological excellence, where timepieces become timeless statements of
+                  elegance. Our collection showcases an unparalleled selection of premium watches, curated from renowned
+                  luxury brands around the globe.
+                </Typography>
+                <Box>
+                  <Button
+                    component={Link}
+                    href="/products"
+                    variant="contained"
+                    color="secondary"
+                    size="large"
+                    endIcon={<IoIosArrowForward />}
+                    sx={{
+                      borderRadius: 6
+                    }}
+                  >
+                    View more
+                  </Button>
+                </Box>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Container>
       </Box>
     </Box>
   );
