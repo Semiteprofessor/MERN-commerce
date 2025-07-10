b97bc66534643779540bd80e10b4533af6425483
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-10 19:55:46 +0100

    Commit

--- frontend/src/components/_main/home/banner/index.jsx
+++ frontend/src/components/_main/home/banner/index.jsx
@@ -1,5 +1,15 @@
-import { Box } from '@mui/material'
-import React from 'react'
+"use client";
+import React from "react";
+import Link from "next/link";
+// mui
+import { Box, Typography, Grid, Button, Container, Stack } from "@mui/material";
+// icons
+import { IoIosArrowForward } from "react-icons/io";
+
+// blur image
+import bannerImg from "../../../../../public/images/banner-3.png";
+// components
+import BlurImage from "@/components/blurImage";
 
 const Banner = () => {
   return (
@@ -11,9 +21,25 @@ const Banner = () => {
         display: { md: "block", xs: "none" },
       }}
     >
-      Banner
+      <Box
+        sx={{
+          mt: 3,
+          py: 12,
+          position: "relative",
+        }}
+      >
+        <BlurImage
+          src={bannerImg}
+          alt="banner-3"
+          placeholder="blur"
+          layout="fill"
+          static
+          sizes="700px"
+          objectFit="cover"
+        />
+      </Box>
     </Box>
   );
-}
+};
 
-export default Banner
+export default Banner;
