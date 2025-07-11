b4ddc68bff760e75ad350420847cd787836b8256
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-11 12:24:11 +0100

    Commit

--- frontend/src/components/_main/home/testimonials/index.jsx
+++ frontend/src/components/_main/home/testimonials/index.jsx
@@ -70,7 +70,88 @@ const Testimonials = () => {
         // display: { xs: 'none', md: 'block' }
       }}
     >
-      Testimonials
+      <Image
+        priority
+        src={bgImage}
+        alt="centered-banner"
+        layout="fill"
+        objectFit="cover"
+        static
+        draggable="false"
+      />
+      <Container maxWidth="xl">
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Typography
+              variant="h3"
+              mb={2}
+              maxWidth={{ xs: "100%", md: 400 }}
+              lineHeight={1.2}
+              textAlign={{ xs: "center", md: "left" }}
+            >
+              Let's explore customer sentiments towards our offerings.
+            </Typography>
+            <Stack sx={{ display: { xs: "none", md: "flex" } }}>
+              <Typography
+                variant="body1"
+                color="text.secondary"
+                mb={2}
+                maxWidth={{ xs: "100%", md: 550 }}
+              >
+                Discover what customers are saying about our products. Dive into
+                the feedback on the quality and performance of our offerings.
+                Gain insights into how our customers perceive our products and
+                their overall satisfaction. Your opinions matter, and we're here
+                to listen..
+              </Typography>
+            </Stack>
+            <Stack
+              direction={"row"}
+              alignItems="center"
+              justifyContent={{ xs: "center", md: "start" }}
+              spacing={3}
+              mt={4}
+            >
+              <Fab
+                color="primary"
+                aria-label="back"
+                size="small"
+                onClick={() => paginate(-1)}
+                // disabled={page === 0}
+                sx={{
+                  bgcolor: isDarkMode ? "primary.main" : "background.paper",
+                  color: isDarkMode ? "common.white" : "primary.main",
+                  zIndex: 99,
+                }}
+              >
+                <IoArrowBackOutline size={24} />
+              </Fab>
+              <Fab
+                color="primary"
+                aria-label="forward"
+                size="small"
+                onClick={() => paginate(1)}
+                sx={{
+                  bgcolor: isDarkMode ? "primary.main" : "background.paper",
+                  color: isDarkMode ? "common.white" : "primary.main",
+                  zIndex: 99,
+                }}
+              >
+                <IoArrowForward size={24} />
+              </Fab>
+            </Stack>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TestimonialCarousel
+              images={data}
+              direction={direction}
+              page={page}
+              imageIndex={imageIndex}
+              paginate={paginate}
+            />
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   );
 };
