fea7f57e9545a5cb61c94116b14217b339937083
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-11 12:39:33 +0100

    Commit

--- frontend/src/components/carousels/testimonial/index.jsx
+++ frontend/src/components/carousels/testimonial/index.jsx
@@ -36,7 +36,54 @@ TestimonialDetailsCarousel.propTypes = {
 };
 
 const TestimonialDetailsCarousel = () => {
-  return <div>TestimonialDetailsCarousel</div>;
+    const { item } = props;
+  return (
+    <div>
+      <Card
+        sx={{
+          p: 2,
+          width: "100%",
+          maxWidth: 500,
+          ml: "auto",
+        }}
+      >
+        <Stack
+          spacing={2}
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+        >
+          <Rating value={5} readOnly size="small" />
+          <Typography variant="body1">{item.reviewdetail}</Typography>
+          <Image
+            priority
+            component={Avatar}
+            src={item.cover.url}
+            placeholder="blur"
+            alt="avatar"
+            responsive
+            static
+            dragable={false}
+            height={80}
+            width={80}
+            sizes="100px"
+            style={{
+              borderRadius: 50,
+            }}
+          />
+          <Stack
+            spacing={0.5}
+            alignItems="center"
+            justifyContent="center"
+            textAlign="center"
+          >
+            <Typography variant="subtitle1">{item.name}</Typography>
+            <Typography variant="body1">{item.jobTitle}</Typography>
+          </Stack>
+        </Stack>
+      </Card>
+    </div>
+  );
 };
 
 export default TestimonialDetailsCarousel;
