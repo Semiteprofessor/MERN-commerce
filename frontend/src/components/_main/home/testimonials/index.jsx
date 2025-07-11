19a235dcedff4d0a718129c4aedd78dda1bf2a47
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-11 12:11:07 +0100

    Commit

--- frontend/src/components/_main/home/testimonials/index.jsx
+++ frontend/src/components/_main/home/testimonials/index.jsx
@@ -22,7 +22,42 @@ import AvatarImg from "../../../../../public/images/avatar.png";
 import TestimonialCarousel from "src/components/carousels/testimonial";
 import { useSelector } from "react-redux";
 
+// data
+const data = [
+    {
+      cover: { url: AvatarImg },
+      name: 'Alex Thompson',
+      jobTitle: 'Software Engineer',
+      reviewdetail:
+        'Exceptional shopping experience! The user-friendly interface and seamless navigation make finding and purchasing products a breeze. As a Marketing Manager, I appreciate the...',
+      reviews: 4
+    },
+    {
+      cover: { url: AvatarImg },
+      name: 'John Thompson',
+      jobTitle: 'Web Developer Engineer',
+      reviewdetail:
+        'Exceptional shopping experience! The user-friendly interface and seamless navigation make finding and purchasing products a breeze. As a Marketing Manager, I appreciate the...',
+      reviews: 5
+    },
+    {
+      cover: { url: AvatarImg },
+      name: 'John Doe',
+      jobTitle: 'Software Engineer',
+      reviewdetail:
+        'Exceptional shopping experience! The user-friendly interface and seamless navigation make finding and purchasing products a breeze. As a Marketing Manager, I appreciate the...',
+      reviews: 3
+    }
+  ];
+  
 const Testimonials = () => {
+    const { themeMode } = useSelector(({ settings }) => settings);
+    const [[page, direction], setPage] = React.useState([0, 0]);
+    const imageIndex = Math.abs(page % data?.length);
+    const paginate = (newDirection) => {
+      setPage([page + newDirection, newDirection]);
+    };
+    const isDarkMode = themeMode === "dark";
   return <div>Testimonials</div>;
 };
 
