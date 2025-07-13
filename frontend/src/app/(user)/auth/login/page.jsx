53f44c63ef764bd223d5d753882d40fc92c315de
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-13 13:36:02 +0100

    Commit

---
+++ frontend/src/app/(user)/auth/login/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+// guard
+import GuestGuard from "src/guards/guest";
+// mui
+import { Card, Stack, Container, Typography } from "@mui/material";
+// components
+import LoginMain from "src/components/_main/auth/login";
+
+// Meta information
+export const metadata = {
+  title:
+    "Login to Nextall | Your Gateway to Seamless Shopping and Secure Transactions",
+  description:
+    "Log in to Nextall for secure access to your account. Enjoy seamless shopping, personalized experiences, and hassle-free transactions. Your trusted portal to a world of convenience awaits. Login now!",
+  applicationName: "Nextall",
+  authors: "Nextall",
+  keywords: "ecommerce, Nextall, Commerce, Login Nextall, LoginFrom Nextall",
+};
+
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
