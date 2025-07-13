582550ae4b4ebed03ac72a567f6c4d6469f0efa0
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-13 13:40:30 +0100

    Commit

---
+++ frontend/src/components/_main/auth/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// component
+import LoginForm from '@/components/forms/login';
+
+const LoginMain = () => {
+  return (
+    <div>LoginMain</div>
+  )
+}
+
+export default LoginMain
