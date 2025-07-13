870b3017ed1de2d4e5d87c6daf1101f561de91f1
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-13 14:53:07 +0100

    Commit

--- frontend/src/app/(user)/auth/login/loading.jsx
+++ frontend/src/app/(user)/auth/login/loading.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginSkeleton from "@/components/_main/skeletons/auth/";
+import LoginSkeleton from "@/components/_main/skeletons/auth/login";
 
 export default function Loading() {
   return <LoginSkeleton />;
