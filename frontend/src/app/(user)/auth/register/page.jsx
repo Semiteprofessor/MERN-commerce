d845ab8e82430bb8959770706fc3985a125f5a66
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-14 15:00:01 +0100

    Commit

---
+++ frontend/src/app/(user)/auth/register/page.jsx
@@ -0,0 +1,24 @@
+// guard
+import GuestGuard from "@/guards/guest";
+// mui
+import { Card, Container, Typography } from "@mui/material";
+// components
+import RegisterMain from "@/components/_main/auth/register";
+
+// Meta information
+export const metadata = {
+  title:
+    "Create Your Nextall Account | Join Us for Exclusive Deals and Seamless Shopping",
+  description:
+    "Register with Nextall today to unlock a world of exclusive deals, personalized recommendations, and secure transactions. Join our community for a seamless shopping experience. Sign up now and elevate your online shopping journey!",
+  applicationName: "Nextall",
+  authors: "Nextall",
+  keywords:
+    "ecommerce, Nextall, Commerce, Register Nextall, RegisterFrom Nextall",
+};
+
+const Register = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
