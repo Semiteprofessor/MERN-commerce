f6ea66a4e1797eb3f249d41435a7cea1ddcb8f53
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-14 15:14:16 +0100

    Commit

--- frontend/src/components/forms/register.jsx
+++ frontend/src/components/forms/register.jsx
@@ -98,31 +98,147 @@ const RegisterForm = () => {
     },
   });
   const { errors, touched, handleSubmit, values, getFieldProps } = formik;
-  return;
-  <FormikProvider value={formik}>
-    <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
-      <Stack spacing={3}>
-        <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+  return (
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+            <Stack gap={0.5} width={1}>
+              <Typography
+                variant="overline"
+                color="text.primary"
+                htmlFor="firstName"
+                component={"label"}
+              >
+                First Name
+              </Typography>
+              <TextField
+                id="firstName"
+                fullWidth
+                type="text"
+                {...getFieldProps("firstName")}
+                error={Boolean(touched.firstName && errors.firstName)}
+                helperText={touched.firstName && errors.firstName}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <IoPerson size={24} />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Stack>
+            <Stack gap={0.5} width={1}>
+              <Typography
+                variant="overline"
+                color="text.primary"
+                htmlFor="lastName"
+                component={"label"}
+              >
+                Last Name
+              </Typography>
+              <TextField
+                fullWidth
+                id="lastName"
+                type="text"
+                {...getFieldProps("lastName")}
+                error={Boolean(touched.lastName && errors.lastName)}
+                helperText={touched.lastName && errors.lastName}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <IoPerson size={24} />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Stack>
+          </Stack>
+          <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+            <Stack gap={0.5} width={1}>
+              <Typography
+                variant="overline"
+                color="text.primary"
+                htmlFor="gender"
+                component={"label"}
+              >
+                Gender
+              </Typography>
+              <TextField
+                id="gender"
+                select
+                fullWidth
+                {...getFieldProps("gender")}
+                error={Boolean(touched.gender && errors.gender)}
+                helperText={touched.gender && errors.gender}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      {values.gender === "male" ? (
+                        <IoMdMale size={24} />
+                      ) : values.gender === "female" ? (
+                        <IoMdFemale size={24} />
+                      ) : (
+                        <FaTransgender />
+                      )}
+                    </InputAdornment>
+                  ),
+                }}
+              >
+                {["Male", "Female", "Other"].map((option) => (
+                  <MenuItem key={option} value={option.toLowerCase()}>
+                    {option}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Stack>
+            <Stack gap={0.5} width={1}>
+              <Typography
+                variant="overline"
+                color="text.primary"
+                htmlFor="phone"
+                component={"label"}
+              >
+                Phone
+              </Typography>
+              <TextField
+                fullWidth
+                id="phone"
+                type="text"
+                {...getFieldProps("phone")}
+                error={Boolean(touched.phone && errors.phone)}
+                helperText={touched.phone && errors.phone}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MdLocalPhone size={24} />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Stack>
+          </Stack>
           <Stack gap={0.5} width={1}>
             <Typography
               variant="overline"
               color="text.primary"
-              htmlFor="firstName"
+              htmlFor="email"
               component={"label"}
             >
-              First Name
+              Email
             </Typography>
             <TextField
-              id="firstName"
+              id="email"
               fullWidth
-              type="text"
-              {...getFieldProps("firstName")}
-              error={Boolean(touched.firstName && errors.firstName)}
-              helperText={touched.firstName && errors.firstName}
+              autoComplete="username"
+              type="email"
+              {...getFieldProps("email")}
+              error={Boolean(touched.email && errors.email)}
+              helperText={touched.email && errors.email}
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
-                    <IoPerson size={24} />
+                    <IoMdMail size={24} />
                   </InputAdornment>
                 ),
               }}
@@ -132,190 +248,90 @@ const RegisterForm = () => {
             <Typography
               variant="overline"
               color="text.primary"
-              htmlFor="lastName"
+              htmlFor="password"
               component={"label"}
             >
-              Last Name
+              Password
             </Typography>
             <TextField
+              id="password"
               fullWidth
-              id="lastName"
-              type="text"
-              {...getFieldProps("lastName")}
-              error={Boolean(touched.lastName && errors.lastName)}
-              helperText={touched.lastName && errors.lastName}
+              autoComplete="current-password"
+              type={showPassword ? "text" : "password"}
+              {...getFieldProps("password")}
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
-                    <IoPerson size={24} />
+                    <MdLock size={24} />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      edge="end"
+                      onClick={() => setShowPassword((prev) => !prev)}
+                    >
+                      {showPassword ? (
+                        <MdOutlineVisibility size={24} />
+                      ) : (
+                        <MdOutlineVisibilityOff size={24} />
+                      )}
+                    </IconButton>
                   </InputAdornment>
                 ),
               }}
+              error={Boolean(touched.password && errors.password)}
+              helperText={touched.password && errors.password}
             />
           </Stack>
-        </Stack>
-        <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
-          <Stack gap={0.5} width={1}>
-            <Typography
-              variant="overline"
-              color="text.primary"
-              htmlFor="gender"
-              component={"label"}
-            >
-              Gender
-            </Typography>
-            <TextField
-              id="gender"
-              select
-              fullWidth
-              {...getFieldProps("gender")}
-              error={Boolean(touched.gender && errors.gender)}
-              helperText={touched.gender && errors.gender}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    {values.gender === "male" ? (
-                      <IoMdMale size={24} />
-                    ) : values.gender === "female" ? (
-                      <IoMdFemale size={24} />
-                    ) : (
-                      <FaTransgender />
-                    )}
-                  </InputAdornment>
-                ),
-              }}
-            >
-              {["Male", "Female", "Other"].map((option) => (
-                <MenuItem key={option} value={option.toLowerCase()}>
-                  {option}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Stack>
-          <Stack gap={0.5} width={1}>
-            <Typography
-              variant="overline"
-              color="text.primary"
-              htmlFor="phone"
-              component={"label"}
-            >
-              Phone
-            </Typography>
-            <TextField
-              fullWidth
-              id="phone"
-              type="text"
-              {...getFieldProps("phone")}
-              error={Boolean(touched.phone && errors.phone)}
-              helperText={touched.phone && errors.phone}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <MdLocalPhone size={24} />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Stack>
-        </Stack>
-        <Stack gap={0.5} width={1}>
-          <Typography
-            variant="overline"
-            color="text.primary"
-            htmlFor="email"
-            component={"label"}
-          >
-            Email
-          </Typography>
-          <TextField
-            id="email"
+          <LoadingButton
             fullWidth
-            autoComplete="username"
-            type="email"
-            {...getFieldProps("email")}
-            error={Boolean(touched.email && errors.email)}
-            helperText={touched.email && errors.email}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <IoMdMail size={24} />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Stack>
-        <Stack gap={0.5} width={1}>
-          <Typography
-            variant="overline"
-            color="text.primary"
-            htmlFor="password"
-            component={"label"}
+            size="large"
+            type="submit"
+            variant="contained"
+            loading={loading}
           >
-            Password
-          </Typography>
-          <TextField
-            id="password"
-            fullWidth
-            autoComplete="current-password"
-            type={showPassword ? "text" : "password"}
-            {...getFieldProps("password")}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <MdLock size={24} />
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    edge="end"
-                    onClick={() => setShowPassword((prev) => !prev)}
-                  >
-                    {showPassword ? (
-                      <MdOutlineVisibility size={24} />
-                    ) : (
-                      <MdOutlineVisibilityOff size={24} />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-            error={Boolean(touched.password && errors.password)}
-            helperText={touched.password && errors.password}
-          />
+            Register
+          </LoadingButton>
         </Stack>
-        <LoadingButton
-          fullWidth
-          size="large"
-          type="submit"
-          variant="contained"
-          loading={loading}
+        <Typography variant="subtitle2" mt={3} textAlign="center">
+          Already have an account? &nbsp;
+          <Link
+            href={`/auth/login${router.query?.redirect ? "?redirect=" + router.query?.redirect : ""}`}
+            component={RouterLink}
+          >
+            Login
+          </Link>
+        </Typography>
+        <Typography
+          variant="body2"
+          align="center"
+          color="text.secondary"
+          mt={2}
         >
-          Register
-        </LoadingButton>
-      </Stack>
-      <Typography variant="subtitle2" mt={3} textAlign="center">
-        Already have an account? &nbsp;
-        <Link
-          href={`/auth/login${router.query?.redirect ? "?redirect=" + router.query?.redirect : ""}`}
-          component={RouterLink}
-        >
-          Login
-        </Link>
-      </Typography>
-      <Typography variant="body2" align="center" color="text.secondary" mt={2}>
-        By registering, I agree to Nextall&nbsp;
-        <Link underline="always" color="text.primary" href="#" fontWeight={700}>
-          Terms
-        </Link>
-        &nbsp;and&nbsp;
-        <Link underline="always" color="text.primary" href="#" fontWeight={700}>
-          Privacy policy
-        </Link>
-        .
-      </Typography>
-    </Form>
-  </FormikProvider>;
+          By registering, I agree to Nextall&nbsp;
+          <Link
+            underline="always"
+            color="text.primary"
+            href="#"
+            fontWeight={700}
+          >
+            Terms
+          </Link>
+          &nbsp;and&nbsp;
+          <Link
+            underline="always"
+            color="text.primary"
+            href="#"
+            fontWeight={700}
+          >
+            Privacy policy
+          </Link>
+          .
+        </Typography>
+      </Form>
+    </FormikProvider>
+  );
 };
 
 export default RegisterForm;
