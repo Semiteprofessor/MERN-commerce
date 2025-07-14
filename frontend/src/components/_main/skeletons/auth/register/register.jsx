80c0e94a7f005494a8dd5aecd0a9f9ad8b1c0cd4
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-14 14:57:53 +0100

    Commit

--- frontend/src/components/_main/skeletons/auth/register/register.jsx
+++ frontend/src/components/_main/skeletons/auth/register/register.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+// mui
+import { Skeleton, Stack, Typography } from "@mui/material";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <Stack spacing={3}>
+      <Stack
+        direction={{ md: "row", xs: "column" }}
+        justifyContent="space-between"
+        spacing={2}
+      >
+        <Skeleton variant="rounded" height={56} width="100%" />
+        <Skeleton variant="rounded" height={56} width="100%" />
+      </Stack>
+      <Stack
+        direction={{ md: "row", xs: "column" }}
+        justifyContent="space-between"
+        spacing={2}
+      >
+        <Skeleton variant="rounded" height={56} width="100%" />
+        <Skeleton variant="rounded" height={56} width="100%" />
+      </Stack>
+      <Skeleton variant="rounded" height={56} />
+      <Skeleton variant="rounded" height={56} />
+      <Skeleton variant="rounded" height={56} />
+      <Typography variant="subtitle2">
+        <Skeleton variant="text" sx={{ margin: "auto" }} />
+      </Typography>
+      <Typography textAlign="center">
+        <Skeleton variant="text" />
+      </Typography>
+    </Stack>
+  );
+};
 
-export default Register
+export default Register;
