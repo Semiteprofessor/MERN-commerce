5e4c3901d5152a5bdba85a7094c528fe3aecbf38
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-14 17:26:56 +0100

    Commit

--- frontend/src/components/_main/auth/forgetPassword.jsx
+++ frontend/src/components/_main/auth/forgetPassword.jsx
@@ -15,7 +15,103 @@ import { CiCircleCheck } from "react-icons/ci";
 import ForgetPasswordForm from "@/components/forms/forgetPassword";
 
 const ForgetPasswordMain = () => {
-  return <div>ForgetPasswordMain</div>;
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [sent, setSent] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { mutate } = useMutation(api.forgetPassword, {
+    onSuccess: () => {
+      setLoading(false);
+      toast.success("Email sent");
+    },
+    onError: (err) => {
+      const message = JSON.stringify(err.response.data.message);
+      setLoading(false);
+      toast.error(message ? JSON.parse(message) : "Something went wrong!");
+    },
+  });
+
+  return (
+    <Container maxWidth="sm">
+      <Card
+        sx={{
+          maxWidth: 560,
+          m: "auto",
+          my: "80px",
+          flexDirection: "column",
+          justifyContent: "center",
+          p: 3,
+          "& .full-width-btn": {
+            mt: 1,
+          },
+        }}
+      >
+        {!sent ? (
+          <>
+            <Typography variant="h3" textAlign="center" paragraph>
+              Forget Password
+            </Typography>
+            <Typography color="text.secondary" mb={5} textAlign="center">
+              Please enter the email address associated with your account and We
+              will email you a link to reset your password.
+            </Typography>
+
+            <ForgetPasswordForm
+              onSent={() => setSent(true)}
+              onGetEmail={(value) => setEmail(value)}
+            />
+
+            <Button
+              fullWidth
+              size="large"
+              onClick={() => router.push("/auth/login")}
+              className="full-width-btn"
+            >
+              back
+            </Button>
+          </>
+        ) : (
+          <Box textAlign="center">
+            <Box
+              sx={{
+                mb: 5,
+                mx: "auto",
+                display: "inline-block",
+              }}
+            >
+              <CiCircleCheck fontSize={160} />
+            </Box>
+
+            <Typography variant="h3" gutterBottom>
+              Request Sent
+            </Typography>
+            <Typography mb={5}>
+              Email has been sent to <strong>{email}</strong>.
+              <br />
+            </Typography>
+            <LoadingButton
+              fullWidth
+              size="large"
+              type="submit"
+              variant="contained"
+              loading={loading}
+              onClick={() => mutate({ email, origin: window.location.origin })}
+            >
+              resend
+            </LoadingButton>
+            <Button
+              size="large"
+              fullWidth
+              onClick={() => router.push("/auth/login")}
+              className="full-width-btn"
+            >
+              back
+            </Button>
+          </Box>
+        )}
+      </Card>
+    </Container>
+  );
 };
 
 export default ForgetPasswordMain;
