4e5bb5ee5966a274ecdcb63d4220de7728b97dc5
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-14 17:30:05 +0100

    Commit

--- frontend/src/components/forms/forgetPassword.jsx
+++ frontend/src/components/forms/forgetPassword.jsx
@@ -18,7 +18,48 @@ import useIsMountedRef from "src/hooks/useIsMountedRef";
 import * as api from "src/services";
 import { useMutation } from "react-query";
 
-const ForgetPasswordForm = () => {
+const ForgetPasswordForm = ({ ...props }) => {
+  const { onSent, onGetEmail } = props;
+  const isMountedRef = useIsMountedRef();
+  const [loading, setloading] = useState(false);
+  const { mutate } = useMutation(api.forgetPassword, {
+    onSuccess: () => {
+      onSent();
+      toast.success("Email sent check inbox");
+      setloading(false);
+    },
+    onError: (err) => {
+      const message = JSON.stringify(err.response.data.message);
+      setloading(false);
+      toast.error(message || "Email incorrect please try again.");
+    },
+  });
+
+  const ResetPasswordSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Enter valid email")
+      .required("Email is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: ResetPasswordSchema,
+    onSubmit: async (values) => {
+      try {
+        setloading(true);
+        onGetEmail(values.email);
+        await mutate({ email: values.email, origin: window.location.origin });
+      } catch (error) {
+        if (isMountedRef.current) {
+          toast.error(error.message);
+        }
+      }
+    },
+  });
+
+  const { errors, touched, handleSubmit, getFieldProps } = formik;
   return <div>ForgetPasswordForm</div>;
 };
 
