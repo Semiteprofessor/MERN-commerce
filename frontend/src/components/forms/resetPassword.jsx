25213e35562989ad2aec2074b163a508ecb16902
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-15 17:49:16 +0100

    Commit

--- frontend/src/components/forms/resetPassword.jsx
+++ frontend/src/components/forms/resetPassword.jsx
@@ -27,7 +27,35 @@ import { useMutation } from "react-query";
 
 // ----------------------------------------------------------------------
 
-const ResetPasswordForm = () => {
+const ResetPasswordForm = ({ ...props }) => {
+  const { token } = props;
+  const { push } = useRouter();
+  const [loading, setloading] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
+  const { mutate } = useMutation(api.resetPassword, {
+    onSuccess: () => {
+      setloading(false);
+      push("/auth/login");
+      toast.success("Password successfully updated.");
+    },
+    onError: (err) => {
+      const message = JSON.stringify(err.response.data.message);
+      setloading(false);
+      toast.error(message || "Reset failed. try again");
+    },
+  });
+
+  const ResetPasswordSchema = Yup.object().shape({
+    password: Yup.string()
+      .min(8, "Short password")
+      .required("Password is required"),
+    confirmPassword: Yup.string().oneOf(
+      [Yup.ref("password")],
+      "Password is not matched"
+    ),
+  });
+
   return <div>ResetPasswordForm</div>;
 };
 
