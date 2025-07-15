3253b90cabca802d02660b274eeec2d982541521
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-15 17:46:59 +0100

    Commit

---
+++ frontend/src/app/(user)/auth/reset-password/[token]/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// guard
+import GuestGuard from "@/guards/guest";
+// mui
+import { Container, Typography, Card } from "@mui/material";
+// component
+import ResetPasswordMain from "@/components/_main/auth/";
+const ResetPassword = () => {
+  return <div>ResetPassword</div>;
+};
+
+export default ResetPassword;
