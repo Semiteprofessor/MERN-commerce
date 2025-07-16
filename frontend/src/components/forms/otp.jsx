15f44e97234b1ce9317ee61c37da577aff174d2c
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-16 18:10:46 +0100

    Commit

--- frontend/src/components/forms/otp.jsx
+++ frontend/src/components/forms/otp.jsx
@@ -15,6 +15,90 @@ import { Box, Card, Stack, Container, Typography } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import { useTheme } from "@mui/material/styles";
 
+// Renderer callback with condition
+const renderer = ({ minutes, seconds }) => {
+  return (
+    <>
+      <Stack
+        direction="row"
+        gap={0.5}
+        sx={{
+          bgcolor: "background.default",
+          border: (theme) => "1px solid " + theme.palette.divider,
+          p: 1,
+          borderRadius: "4px",
+          maxWidth: 100,
+          mx: "auto",
+          mt: 2,
+          color: "text.primary",
+        }}
+      >
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          gap={0}
+          sx={{
+            height: 28,
+            width: 28,
+            borderRadius: "2px",
+          }}
+        >
+          <Typography
+            variant="body1"
+            color="text.primary"
+            fontWeight={800}
+            fontSize={14}
+            lineHeight={1}
+          >
+            {minutes}
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.primary"
+            fontSize={8}
+            fontWeight={400}
+            lineHeight={1}
+          >
+            MIN
+          </Typography>
+        </Stack>
+        <Typography variant="body1" color="common.white">
+          :
+        </Typography>
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          gap={0}
+          sx={{
+            height: 28,
+            width: 28,
+            borderRadius: "2px",
+          }}
+        >
+          <Typography
+            variant="body1"
+            color="text.primary"
+            fontWeight={800}
+            fontSize={14}
+            lineHeight={1}
+          >
+            {seconds}
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.primary"
+            fontSize={8}
+            fontWeight={400}
+            lineHeight={1}
+          >
+            SEC
+          </Typography>
+        </Stack>
+      </Stack>
+    </>
+  );
+};
+
 const VerifyOTPForm = () => {
   return <div>VerifyOTPForm</div>;
 };
