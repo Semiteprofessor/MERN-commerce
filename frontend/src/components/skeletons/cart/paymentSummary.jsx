d7a8e754d96c8e39d73be975ea896f8d15d49a53
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-17 11:49:47 +0100

    Commit

---
+++ frontend/src/components/skeletons/cart/paymentSummary.jsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+// mui
+import {
+  Card,
+  CardContent,
+  Typography,
+  Stack,
+  Divider,
+  Skeleton,
+} from "@mui/material";
+
+const PaymentSummarySkeleton = () => {
+  return <div>PaymentSummarySkeleton</div>;
+};
+
+export default PaymentSummarySkeleton;
