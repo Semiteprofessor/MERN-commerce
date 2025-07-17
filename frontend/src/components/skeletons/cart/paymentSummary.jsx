b05708334bc126b14aa05ec3e21ba395094d3d18
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-17 16:15:11 +0100

    Commit

--- frontend/src/components/skeletons/cart/paymentSummary.jsx
+++ frontend/src/components/skeletons/cart/paymentSummary.jsx
@@ -11,7 +11,58 @@ import {
 } from "@mui/material";
 
 const PaymentSummarySkeleton = () => {
-  return <div>PaymentSummarySkeleton</div>;
+  return (
+    <Card>
+      <CardContent sx={{ py: 2 }}>
+        <Typography variant="h4" mb={1}>
+          <Skeleton variant="text" width={180} />
+        </Typography>
+        <Stack spacing={0} mt={1} mb={2}>
+          <Stack
+            direction="row"
+            alignItem="center"
+            justifyContent="space-between"
+            spacing={2}
+          >
+            <Typography variant="subtitle2" color="text.secondary">
+              <Skeleton variant="text" width={80} />
+            </Typography>
+            <Typography variant="subtitle2">
+              <Skeleton variant="text" width={80} />
+            </Typography>
+          </Stack>
+          <Stack
+            direction="row"
+            alignItem="center"
+            justifyContent="space-between"
+            spacing={2}
+          >
+            <Typography variant="subtitle2" color="text.secondary">
+              <Skeleton variant="text" width={80} />
+            </Typography>
+            <Typography variant="subtitle2">
+              <Skeleton variant="text" width={80} />
+            </Typography>
+          </Stack>
+        </Stack>
+        <Divider />
+        <Stack
+          direction="row"
+          alignItem="center"
+          justifyContent="space-between"
+          spacing={2}
+          mt={2}
+        >
+          <Typography variant="subtitle1">
+            <Skeleton variant="text" width={80} />
+          </Typography>
+          <Typography variant="subtitle1">
+            <Skeleton variant="text" width={80} />
+          </Typography>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default PaymentSummarySkeleton;
