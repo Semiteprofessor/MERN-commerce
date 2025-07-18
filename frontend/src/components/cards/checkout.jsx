c7cdcc64f542ae89956a3dadba8d3d52de4ef89a
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-18 19:58:00 +0100

    Commit

--- frontend/src/components/cards/checkout.jsx
+++ frontend/src/components/cards/checkout.jsx
@@ -31,19 +31,95 @@ const CheckoutCard = () => {
   const { onDelete, onIncreaseQuantity, onDecreaseQuantity, cart } = props;
   const cCurrency = useCurrencyConvert();
   const fCurrency = useCurrencyFormatter();
-  return;
-  <Box
-    sx={{
-      "& .card-main": {
-        p: 2,
-        borderWidth: "1px 0 0 0",
-        "& .delete-icon": {
-          fontSize: 20,
+  return (
+    <Box
+      sx={{
+        "& .card-main": {
+          p: 2,
+          borderWidth: "1px 0 0 0",
+          "& .delete-icon": {
+            fontSize: 20,
+          },
         },
-      },
-      display: { sm: "none", xs: "block" },
-    }}
-  ></Box>;
+        display: { sm: "none", xs: "block" },
+      }}
+    >
+      {cart.map((product) => {
+        const {
+          sku,
+          name,
+          brand,
+          priceSale,
+          color,
+          size,
+          image,
+          quantity,
+          available,
+        } = product;
+
+        return (
+          <Card className="card-main" key={Math.random()}>
+            <Stack direction="row" alignItems="center">
+              <ThumbImgStyle>
+                <BlurImage priority fill alt="product image" src={image} />
+              </ThumbImgStyle>
+              <Box sx={{ display: "contents" }}>
+                <Typography
+                  variant="h5"
+                  color="text.primary"
+                  noWrap
+                  textOverflow="ellipsis"
+                >
+                  {name}
+                </Typography>
+                <Typography variant="body2" color="text.primary" mb={0.5}>
+                  {brand?.name}
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" justifyContent="space-between">
+              <Box mt={1}>
+                {/* <Typography variant="body2" color="text.primary" mb={0.5}>
+                  {name}
+                </Typography> */}
+                <Typography variant="body2" color="text.primary" mb={0.5}>
+                  <b>Color:</b> {color}
+                  {/* { formatNumbers(Number(priceSale), Number(unitRate))} */}
+                </Typography>
+                <Typography variant="body2" color="text.primary" mb={0.5}>
+                  <b>Size:</b>
+                  {size}
+                </Typography>
+                <Typography variant="body2" color="text.primary" mb={0.5}>
+                  <b>subtotal:</b> {fCurrency(cCurrency(priceSale * quantity))}
+                  {/* { formatNumbers(Number(priceSale), Number(unitRate))} */}
+                </Typography>
+                {/* <Typography variant="body2" color="text.primary" mb={0.5}>
+                  <b>Total:</b> $ {priceSale * quantity}
+                  
+                </Typography> */}
+              </Box>
+              <Box textAlign="right">
+                <Incrementer
+                  quantity={quantity}
+                  available={available}
+                  onDecrease={() => onDecreaseQuantity(sku)}
+                  onIncrease={() => onIncreaseQuantity(sku)}
+                />
+                <IconButton
+                  color="primary"
+                  onClick={() => onDelete(sku)}
+                  sx={{ mt: 1 }}
+                >
+                  <MdDeleteOutline className="delete-icon" />
+                </IconButton>
+              </Box>
+            </Stack>
+          </Card>
+        );
+      })}
+    </Box>
+  );
 };
 
 export default CheckoutCard;
