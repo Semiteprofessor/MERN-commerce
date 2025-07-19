b6fd91990e74e4573219ee75881dc5fdf9c515c7
Taiwo Olapade <semiteprofessor@gmail.com>
2025-07-19 22:56:08 +0100

    Commit

---
+++ frontend/src/components/_main/products/filters/index.jsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import PropTypes from "prop-types";
+
+// mui
+import { Card, Box, Typography, IconButton, Divider } from "@mui/material";
+// icons
+import { MdClear } from "react-icons/md";
+
+// components
+import BrandsFilter from "./brands";
+import GenderFilter from "./gender";
+import ColorsFilter from "./colors";
+import SizesFilter from "./sizes";
+import PriceRange from "./price";
+import Brands from "src/components/_main/skeletons/products/filters/brands";
+import Gender from "src/components/_main/skeletons/products/filters/gander";
+import Color from "src/components/_main/skeletons/products/filters/colors";
+import Sizes from "src/components/_main/skeletons/products/filters/sizes";
+// api
+import * as api from "src/services";
+import { useQuery } from "react-query";
+
+Filter.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  pathname: PropTypes.string.isRequired,
+  shop: PropTypes.object,
+  category: PropTypes.object,
+  subCategory: PropTypes.object,
+};
+
+const Filter = ({ ...props }) => {
+  return <div>Filter</div>;
+};
+
+export default Filter;
